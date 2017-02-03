7bcbf9119ec40fcbc0a9a3f2d4be252bd8a5aee4
Jose Vega <jose.vega@rivetlogic.com>
2017-02-03 14:23:59 -0600

    [studio-ui] Add button to sync from repository #438

---
+++ static-assets/components/cstudio-contextual-nav/wcm-site-dropdown-mods/syncrepo.js
@@ -0,0 +1,42 @@
+var YDom = YAHOO.util.Dom;
+var YEvent = YAHOO.util.Event;
+
+
+/**
+ * PreviewSync
+ */
+CStudioAuthoring.ContextualNav.SyncFromRepo = CStudioAuthoring.ContextualNav.SyncFromRepo || {
+
+        /**
+         * initialize module
+         */
+        initialize: function(config) {
+
+            console.log("test");
+
+            if(config.name == "syncrepo") {
+                this.initialized = true;
+                var dropdownInnerEl = config.containerEl;
+
+                var parentFolderEl = document.createElement("div");
+                parentFolderEl.style.paddingTop = "8px";
+                var parentFolderLinkEl = document.createElement("a");
+                parentFolderEl.appendChild(parentFolderLinkEl);
+                YDom.addClass(parentFolderLinkEl, "acn-syncrepo");
+
+                parentFolderLinkEl.id = "syncrepo";
+                parentFolderLinkEl.innerHTML = CMgs.format(siteDropdownLangBundle, "syncrepo");
+                parentFolderLinkEl.onclick = function() {
+                    CStudioAuthoring.Service.syncFromRepo(CStudioAuthoringContext.site, {
+                        success: function(result) {
+                            alert("Synchronization from Repository initiated.");
+                        },
+                        failure: function() {}
+                    });
+                };
+
+                dropdownInnerEl.appendChild(parentFolderEl);
+            }
+        }
+    }
+CStudioAuthoring.Module.moduleLoaded("syncrepo", CStudioAuthoring.ContextualNav.SyncFromRepo);
