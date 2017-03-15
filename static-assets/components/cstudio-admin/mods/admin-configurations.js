4b7347c78b7420b15bcf45f04eade97d3b3e20ea
Veronica Estrada <mariave011@gmail.com>
2017-03-15 14:31:24 -0600

    https://github.com/craftercms/craftercms/issues/469

--- static-assets/components/cstudio-admin/mods/admin-configurations.js
+++ static-assets/components/cstudio-admin/mods/admin-configurations.js
@@ -141,6 +141,7 @@ CStudioAuthoring.Module.requireModule(
 						var getConfigCb = {
 							success: function(response) {
 								editor.setValue(response.responseText);
+                                editor.clearHistory();
 								CStudioAdminConsole.Tool.AdminConfig.prototype.expandEditor(editor);
 							},
 							failure: function() {
