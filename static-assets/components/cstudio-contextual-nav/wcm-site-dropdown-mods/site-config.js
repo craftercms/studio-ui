3930a6bb4810a470bf45b0d2942aab6df89b1fc7
jvega-crafter <jose.vega@craftersoftware.com>
2017-06-02 13:15:35 -0600

    Updated sidebar tree to use CStudioAuthoring.Utils.createIcon since now we use HTMLNode icons on trees

--- static-assets/components/cstudio-contextual-nav/wcm-site-dropdown-mods/site-config.js
+++ static-assets/components/cstudio-contextual-nav/wcm-site-dropdown-mods/site-config.js
@@ -34,29 +34,10 @@ CStudioAuthoring.ContextualNav.SiteConfig = CStudioAuthoring.ContextualNav.SiteC
             var confLabel = moduleConfig.label.toLowerCase().replace(/\s/g,'');
             var label = CMgs.format(siteDropdownLangBundle, confLabel) == confLabel ? moduleConfig.label : CMgs.format(siteDropdownLangBundle, confLabel);
 
-            parentFolderLinkEl.innerHTML = label;
+            var icon = CStudioAuthoring.Utils.createIcon(moduleConfig, "fa-sliders");
+            parentFolderLinkEl.appendChild(icon);
 
-            if(moduleConfig.icon){
-                if (moduleConfig.icon.class){
-                    parentFolderLinkEl.className += " acn-parent-folder custom-icon " + moduleConfig.icon.class;
-                }
-
-                if(moduleConfig.icon.styles){
-                    var iconStyles = moduleConfig.icon.styles;
-
-                    if (iconStyles){
-                        var styles = "";
-                        for (var key in iconStyles) {
-                            if (iconStyles.hasOwnProperty(key)) {
-                                styles += key + ":" + iconStyles[key] + ";";
-                            }
-                        }
-                        $("head").append("<style>#" + parentFolderLinkEl.id + ":before{" + styles + "}</style>");
-                    }
-                }
-            }else{
-                parentFolderLinkEl.className += " acn-parent-folder custom-icon fa-sliders";
-            }
+            parentFolderLinkEl.innerHTML += label;
 
             parentFolderLinkEl.onclick = function() {
             document.location = CStudioAuthoringContext.authoringAppBaseUri +
