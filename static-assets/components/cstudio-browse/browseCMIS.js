29d865a25f5259cbd7a07124bd3fa6b4da95b954
Veronica Estrada <mariave011@gmail.com>
2017-07-13 16:52:41 -0600

    https://github.com/craftercms/craftercms/issues/836 - Getting the content on browse

--- static-assets/components/cstudio-browse/browseCMIS.js
+++ static-assets/components/cstudio-browse/browseCMIS.js
@@ -15,6 +15,28 @@
             browseLangBundle = CMgs.getBundle("browse", CStudioAuthoringContext.lang);
     };
 
+    CStudioBrowseCMIS.getContent = function(type) {
+        var path = CStudioAuthoring.Utils.getQueryParameterByName("path"),
+            repoId = CStudioAuthoring.Utils.getQueryParameterByName("repoId"),
+            site = CStudioAuthoring.Utils.getQueryParameterByName("site");
+        var serviceCallback = {
+            success: function(response) {
+                console.log(response); //TODO
+            },
+
+            failure: function(response) {
+                console.log(response); //TODO
+            }
+        };
+        if(type == "browse"){
+            CStudioAuthoring.Service.getCMISContentByBrowser(site, repoId, path, serviceCallback);
+        }else{
+            CStudioAuthoring.Service.getCMISContentBySearch(site, repoId, path, serviceCallback);
+        }
+
+
+    };
+
 
     window.CStudioBrowseCMIS = CStudioBrowseCMIS;
 
