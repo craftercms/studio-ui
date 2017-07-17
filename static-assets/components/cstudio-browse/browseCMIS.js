430e34455cf4ff33257eb3fab82a4fcb1343eb90
Veronica Estrada <mariave011@gmail.com>
2017-07-17 13:23:41 -0600

    https://github.com/craftercms/craftercms/issues/836 - Tabs & Search

--- static-assets/components/cstudio-browse/browseCMIS.js
+++ static-assets/components/cstudio-browse/browseCMIS.js
@@ -14,14 +14,14 @@
     };
 
     CStudioBrowseCMIS.bindEvents = function() {
-        $('.tabs .tab-links a').on('click', function (e) {
-            var currentAttrValue = jQuery(this).attr('href');
+        $(".tabs .tab-links a").on("click", function (e) {
+            var currentAttrValue = jQuery(this).attr("href");
 
             // Show/Hide Tabs
-            $('.tabs ' + currentAttrValue).show().siblings().hide();
+            $(".tabs " + currentAttrValue).show().siblings().hide();
 
             // Change/remove current tab to active
-            $(this).parent('li').addClass('active').siblings().removeClass('active');
+            $(this).parent("li").addClass("active").siblings().removeClass("active");
 
             e.preventDefault();
         });
