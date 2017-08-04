b00347e17165dab77d75a03a69c239ca9e68a070
Veronica Estrada <mariave011@gmail.com>
2017-08-04 09:43:02 -0600

    https://github.com/craftercms/craftercms/issues/1189 - [studio-ui] CMIS data sources should allow form creator to constrain Link and Clone options #1189

--- static-assets/components/cstudio-forms/data-sources/CMIS-repo.js
+++ static-assets/components/cstudio-forms/data-sources/CMIS-repo.js
@@ -15,6 +15,16 @@ CStudioForms.Datasources.CMISRepo= CStudioForms.Datasources.CMISRepo ||
             if(properties[i].name === "studioPath") {
                 this.studioPath = properties[i].value;
             }
+            if(properties[i].name === "allowedOperations") {
+                //this.allowedOperations = properties[i].value;
+                var propValues = JSON.parse(properties[i].value);
+
+                for(var x = 0; x < propValues.length; x++) {
+                    if(propValues[x].selected){
+                        this.allowedOperations = propValues[x].value;
+                    }
+                }
+            }
         }
 
         return this;
@@ -24,7 +34,7 @@ YAHOO.extend(CStudioForms.Datasources.CMISRepo, CStudioForms.CStudioFormDatasour
 
     add: function(control) {
         var _self = this;
-        CStudioAuthoring.Operations.openCMISBrowse(_self.repoId, _self.repoPath, _self.studioPath, "select", true, {
+        CStudioAuthoring.Operations.openCMISBrowse(_self.repoId, _self.repoPath, _self.studioPath, _self.allowedOperations, "select", true, {
             success: function(searchId, selectedTOs) {
 
                 var cb = function(repositories){
@@ -93,7 +103,25 @@ YAHOO.extend(CStudioForms.Datasources.CMISRepo, CStudioForms.CStudioFormDatasour
         return [
             { label: CMgs.format(langBundle, "repositoryPath"), name: "repoPath", type: "string" },
             { label: CMgs.format(langBundle, "repositoryId"), name: "repoId", type: "string" },
-            { label: CMgs.format(langBundle, "studioPath"), name: "studioPath", type: "string" }
+            { label: CMgs.format(langBundle, "studioPath"), name: "studioPath", type: "string" },
+            {
+                label: CMgs.format(langBundle, "allowedOperations"),
+                name: "allowedOperations",
+                type: "dropdown",
+                defaultValue: [{
+                    value: "value_both",
+                    label: CMgs.format(langBundle, "cloneAndLink"),
+                    selected: true
+                }, {
+                    value: "value_clone",
+                    label: CMgs.format(langBundle, "clone"),
+                    selected: false
+                }, {
+                    value: "value_link",
+                    label: CMgs.format(langBundle, "link"),
+                    selected: false
+                }]
+            }
         ];
     },
 
