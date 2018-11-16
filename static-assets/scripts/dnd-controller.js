e454a7aee714c3db13f24bf2e7302f1a894ae00a
Veronica Estrada <mariave011@gmail.com>
2018-11-16 16:12:45 -0600

    https://github.com/craftercms/craftercms/issues/2381 - js exception - on Preview Tools > Page Components expansion #2381

--- static-assets/scripts/dnd-controller.js
+++ static-assets/scripts/dnd-controller.js
@@ -487,14 +487,16 @@ define('dnd-controller', ['crafter', 'jquery', 'jquery-ui', 'animator', 'communi
             html.push('<sdiv class="studio-category">');
             html.push('<sh2 class="studio-category-name studio-category-name-collapse">'+category.label+'</sh2>');
             html.push('<sul>');
-            if(category.components.length){
-                $.each(category.components, function (j, component) {
+            if(category.components){
+                if(category.components.length){
+                    $.each(category.components, function (j, component) {
+                        html.push(crafter.String(COMPONENT_TPL)
+                            .fmt(component.path, component.type, component.label));
+                    });
+                }else{
                     html.push(crafter.String(COMPONENT_TPL)
-                        .fmt(component.path, component.type, component.label));
-                });
-            }else{
-                html.push(crafter.String(COMPONENT_TPL)
-                    .fmt(category.components.path, category.components.type, category.components.label));
+                        .fmt(category.components.path, category.components.type, category.components.label));
+                }
             }
             html.push('</sul>');
             html.push('</sdiv>');
