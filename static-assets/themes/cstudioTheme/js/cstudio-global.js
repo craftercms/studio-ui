b6f437e0bc65862d5071746bd6d9bbf805f896f8
Dejan Brkic <dejan.brkic@craftersoftware.com>
2019-01-10 19:21:01 +0100

    Copyright updated

--- static-assets/themes/cstudioTheme/js/cstudio-global.js
+++ static-assets/themes/cstudioTheme/js/cstudio-global.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2007-2019 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* Functions for onload events */
 /* These say the are global but want to double check if they can be removed and applied exactly that's better.
  * so far it looks like this is only used on the login page
