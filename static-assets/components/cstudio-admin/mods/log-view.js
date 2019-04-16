4b8314fe32982cd3b6b251adaa806f3a10f90c58
jvega190 <jose.vega@craftersoftware.com>
2019-04-16 13:44:13 -0600

    #3076 - Updates from review - indentation

--- static-assets/components/cstudio-admin/mods/log-view.js
+++ static-assets/components/cstudio-admin/mods/log-view.js
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-CStudioAdminConsole.Tool.LogView = CStudioAdminConsole.Tool.LogView ||  function(config, el)  {
+CStudioAdminConsole.Tool.LogView = CStudioAdminConsole.Tool.LogView || function (config, el) {
 	this.containerEl = el;
 	this.config = config;
 	this.types = [];
@@ -26,21 +26,20 @@ CStudioAdminConsole.Tool.LogView = CStudioAdminConsole.Tool.LogView ||  function
 * Overarching class that drives the content type tools
 */
 YAHOO.extend(CStudioAdminConsole.Tool.LogView, CStudioAdminConsole.Tool, {
-renderWorkarea: function() {
-	var workareaEl = document.getElementById("cstudio-admin-console-workarea"),
-		auditUrl = '/studio/#/log?iframe=true&site=' + CStudioAuthoringContext.siteId,
-		actions = [];
+	renderWorkarea: function () {
+		var workareaEl = document.getElementById("cstudio-admin-console-workarea"),
+			auditUrl = '/studio/#/log?iframe=true&site=' + CStudioAuthoringContext.siteId,
+			actions = [];
 
-	workareaEl.innerHTML =
-		'<div class="iframe-container" style="position: relative; top: 50px; height: calc(100vh - 50px);">' +
-			'<iframe src="'+ auditUrl +'" style="width: 100%; height: 100%;"></iframe>' +
-		'</div>';
+		workareaEl.innerHTML =
+			'<div class="iframe-container" style="position: relative; top: 50px; height: calc(100vh - 50px);">' +
+			'<iframe src="' + auditUrl + '" style="width: 100%; height: 100%;"></iframe>' +
+			'</div>';
 
-	CStudioAuthoring.ContextualNav.AdminConsoleNav.initActions(actions);
-}
-
+		CStudioAuthoring.ContextualNav.AdminConsoleNav.initActions(actions);
+	}
 });
 
 
 
-CStudioAuthoring.Module.moduleLoaded("cstudio-console-tools-log-view",CStudioAdminConsole.Tool.LogView);
+CStudioAuthoring.Module.moduleLoaded("cstudio-console-tools-log-view", CStudioAdminConsole.Tool.LogView);
