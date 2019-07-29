b74cbe4995aeaec22825779d123eb6b6752be08b
jvega190 <jose.vega@craftersoftware.com>
2019-07-29 08:37:35 -0600

    [studio-ui] Group management issues #3312

--- static-assets/scripts/main.js
+++ static-assets/scripts/main.js
@@ -1894,6 +1894,22 @@
         };
     });
 
+    app.directive("stResetSearch", function() {
+      return {
+        restrict: 'EA',
+        require: '^stTable',
+        link: function(scope, element, attrs, ctrl) {
+          return element.bind('click', function() {
+            return scope.$evalAsync(function() {
+              var tableState;
+              tableState = ctrl.tableState();
+              tableState.search.predicateObject = {};
+              tableState.pagination.start = 0;
 
-
+              return ctrl.pipe();
+            });
+          });
+        }
+      };
+    });
 })(angular);
