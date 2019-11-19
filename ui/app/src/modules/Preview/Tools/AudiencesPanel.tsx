6f0bbd28536e84bc285a4eca247f90c964edf74b
Veronica Estrada <mariave011@gmail.com>
2019-11-19 14:46:09 -0600

    Reactify Targeting dialog

--- ui/app/src/modules/Preview/Tools/AudiencesPanel.tsx
+++ ui/app/src/modules/Preview/Tools/AudiencesPanel.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import ToolPanel from './ToolPanel';
-import { defineMessages } from 'react-intl';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 
 const translations = defineMessages({
   audiencesPanel: {
@@ -10,12 +11,73 @@ const translations = defineMessages({
   }
 });
 
-export default function AudiencesPanel() {
+const useStyles = makeStyles((theme) => createStyles({
+  iframe: {
+    width: '100%',
+    maxWidth: '100%',
+    border: 'none',
+    height: '100%',
+    transition: 'width .25s ease, height .25s ease'
+  },
+  iframeWithBorder: {
+    borderRadius: 20,
+    borderColor: '#555',
+  },
+  iframeWithBorderLandscape: {
+    borderWidth: '10px 50px'
+  },
+  iframeWithBorderPortrait: {
+    borderWidth: '50px 10px'
+  },
+  hostContainer: {
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: '#f3f3f3',
+    height: '100%',
+    maxHeight: 'calc(100% - 64px)',
+    overflow: 'auto',
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  }
+}));
+
+interface AudiencesPanelProps {
+
+}
+
+export function AudiencesPanelUI(props: AudiencesPanelProps) {
+
+  const classes = useStyles({});
+
+  const {
+  } = props;
+
   return (
     <ToolPanel title={translations.audiencesPanel}>
       <Typography component="h2" variant="subtitle1" style={{ padding: '10px' }}>
-        Audiences Panel
+        <FormattedMessage
+          id="craftercms.ice.audiences.audiencesPanel"
+          defaultMessage={`Audiences Panel`}
+        />
       </Typography>
     </ToolPanel>
   );
+
+}
+
+export default function AudiencesPanel() {
+
+  const classes = useStyles({});
+
+  return (
+    <div>
+      <AudiencesPanelUI
+      />
+    </div>
+  );
+
 }
