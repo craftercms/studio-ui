ae3dfc27404ab38d7ce0b8a0a433d24d9ce945b4
rart <royart@me.com>
2020-01-02 19:34:41 +0100

    Updates in PasswordTextField to support showing or not the visibility switcher, supporting starting with visibility switched on, translating the button label and simplifying API

--- ui/app/src/components/Controls/PasswordTextField.tsx
+++ ui/app/src/components/Controls/PasswordTextField.tsx
@@ -15,59 +15,57 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import TextField from "@material-ui/core/TextField";
-import React, { useState } from "react";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import IconButton from "@material-ui/core/IconButton";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import TextField, { TextFieldProps } from '@material-ui/core/TextField';
+import React, { useState } from 'react';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { defineMessages, useIntl } from 'react-intl';
 
-interface PasswordTextFieldProps {
-  id: string;
-  fullWidth: boolean;
-  name: string;
-  label: string;
-  required: boolean;
-  onKeyPress(event: React.KeyboardEvent): any;
-  onChange(event: React.ChangeEvent): any;
-  value: string;
-  error: boolean;
-  helperText: string;
-}
+type PasswordTextFieldProps = TextFieldProps & {
+  visibilitySwitch?: boolean,
+  initialVisible?: boolean
+};
+
+const translations = defineMessages({
+  toggleVisibilityButtonText: {
+    id: 'passwordTextField.toggleVisibilityButtonText',
+    defaultMessage: 'toggle password visibility'
+  }
+});
 
 export default function PasswordTextField(props: PasswordTextFieldProps) {
-  const {name, label, required, onKeyPress, onChange, value, error, helperText, fullWidth} = props;
-  const [showPassword, setShowPassword] = useState(false);
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
+
+  const { visibilitySwitch = true, initialVisible = false } = props;
+  const { formatMessage } = useIntl();
+  const [showPassword, setShowPassword] = useState(initialVisible);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
 
   return (
     <TextField
-      id={name}
-      fullWidth={fullWidth}
-      name={name}
+      {...props}
       type={showPassword ? 'text' : 'password'}
-      label={label}
-      required={required}
-      onKeyPress={onKeyPress}
-      onChange={onChange}
-      value={value}
-      error={error}
-      helperText={helperText}
-      InputProps={{
-        endAdornment: (
-          <InputAdornment position="end">
-            <IconButton
-              edge="end"
-              aria-label="toggle password visibility"
-              onClick={handleClickShowPassword}
-            >
-              {showPassword ? <VisibilityOff/> : <Visibility/>}
-            </IconButton>
-          </InputAdornment>
-        ),
-      }}
+      InputProps={
+        visibilitySwitch ? {
+          ...props.InputProps,
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                edge="end"
+                aria-label={formatMessage(translations.toggleVisibilityButtonText)}
+                onClick={handleClickShowPassword}
+              >
+                {showPassword ? <VisibilityOff/> : <Visibility/>}
+              </IconButton>
+            </InputAdornment>
+          )
+        } : props.InputProps
+      }
     />
   )
 }
+
+PasswordTextField.defaultProps = {
+  type: 'password'
+};
