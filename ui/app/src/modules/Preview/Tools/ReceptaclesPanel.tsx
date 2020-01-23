592112d1b468e479830eb5549a4fd2d20818b156
Aaron <aronchj7@live.com>
2020-01-23 08:51:07 -0600

    adding contentType select on ReceptaclesPanel

--- ui/app/src/modules/Preview/Tools/ReceptaclesPanel.tsx
+++ ui/app/src/modules/Preview/Tools/ReceptaclesPanel.tsx
@@ -16,7 +16,7 @@
 
 import React, { useEffect } from 'react';
 import ToolPanel from './ToolPanel';
-import { defineMessages } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
 import { getHostToGuestBus } from "../previewContext";
 import { SHOW_RECEPTACLES_BY_CONTENT_TYPE } from "../../../state/actions/preview";
 import { useSelection } from '../../../utils/hooks';
@@ -28,50 +28,72 @@ import ListItemText from '@material-ui/core/ListItemText';
 import List from '@material-ui/core/List';
 import Avatar from "@material-ui/core/Avatar";
 import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import ContentType from "../../../models/ContentType";
 
 const translations = defineMessages({
   receptaclesPanel: {
     id: 'craftercms.ice.receptacles.title',
     defaultMessage: 'Receptacles'
-  }
+  },
+  selectContentType: {
+    id: 'craftercms.ice.receptacles.selectContentType',
+    defaultMessage: 'Select content type'
+  },
 });
 
-const useStyles = makeStyles((theme) => createStyles({}));
+const useStyles = makeStyles((theme) => createStyles({
+  select: {
+    width: '100%',
+    marginTop: '15px'
+  },
+}));
 
 export default function ReceptaclesPanel() {
   const classes = useStyles({});
   const hostToGuest$ = getHostToGuestBus();
   const receptaclesBranch = useSelection(state => state.preview.receptacles);
   const receptacles = receptaclesBranch.byId ? Object.values(receptaclesBranch.byId) : null;
+  const contentTypesBranch = useSelection(state => state.contentTypes);
+  const contentTypes = contentTypesBranch.byId ? Object.values(contentTypesBranch.byId).filter((contentType) => contentType.type === 'component') : null;
+  const { formatMessage } = useIntl();
 
   useEffect(() => {
     console.log('SHOW_RECEPTACLES_BY_CONTENT_TYPE');
-    hostToGuest$.next({
-      type: SHOW_RECEPTACLES_BY_CONTENT_TYPE,
-      payload: receptaclesBranch.selectedContentType
-    });
-  }, [hostToGuest$, receptaclesBranch.selectedContentType]);
+    showReceptaclesByContentType(receptaclesBranch.selectedContentType);
+  }, [receptaclesBranch.selectedContentType]);
 
   const onSelectedDropZone = (receptacle: Receptacle) => {
     console.log(receptacle);
   };
 
+  function handleSelectChange(value: string) {
+    showReceptaclesByContentType(value);
+  }
+
+  const showReceptaclesByContentType = (contentType: string) => {
+    hostToGuest$.next({
+      type: SHOW_RECEPTACLES_BY_CONTENT_TYPE,
+      payload: contentType
+    });
+  };
+
   return (
     <ToolPanel title={translations.receptaclesPanel}>
-      {/*<Select*/}
-      {/*  value={receptaclesContentType}*/}
-      {/*  displayEmpty*/}
-      {/*  className={classes.Select}*/}
-      {/*  onChange={(event: any) => handleSelectChange(event.target.value)}*/}
-      {/*  disabled={isFetching}*/}
-      {/*>*/}
-      {/*  <MenuItem value="" disabled>{formatMessage(translations.selectContentType)}</MenuItem>*/}
-      {/*  {*/}
-      {/*    contentTypes.map((contentType: ContentType, i: number) => {*/}
-      {/*      return <MenuItem value={contentType.id} key={i}>{contentType.name}</MenuItem>*/}
-      {/*    })*/}
-      {/*  }*/}
-      {/*</Select>*/}
+      <Select
+        value={receptaclesBranch.selectedContentType}
+        displayEmpty
+        className={classes.select}
+        onChange={(event: any) => handleSelectChange(event.target.value)}
+      >
+        <MenuItem value="" disabled>{formatMessage(translations.selectContentType)}</MenuItem>
+        {
+          contentTypes.map((contentType: ContentType, i: number) => {
+            return <MenuItem value={contentType.id} key={i}>{contentType.name}</MenuItem>
+          })
+        }
+      </Select>
       <List>
         {
           receptacles && receptacles.map((receptacle: Receptacle) =>
