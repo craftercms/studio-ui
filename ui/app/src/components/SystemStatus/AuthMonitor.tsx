50703f4d63b27a51977cbc95ac3ea470f9591c7f
rart <royart@me.com>
2020-02-14 00:30:12 +0100

    Session validation response format correction and check session every minute

--- ui/app/src/components/SystemStatus/AuthMonitor.tsx
+++ ui/app/src/components/SystemStatus/AuthMonitor.tsx
@@ -159,7 +159,7 @@ export default function AuthMonitor() {
   useEffect(() => {
     if (active) {
       setPassword('');
-      const sub = interval(300000).pipe(
+      const sub = interval(60000).pipe(
         switchMap(() => validateSession())
       ).subscribe((active) => {
         setState({ active: active });
