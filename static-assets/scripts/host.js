77561bf664d9567f75c3a346db89b16b7dc7c190
Aaron <aronchj7@live.com>
2020-02-21 08:55:32 -0600

    removing console.log

--- static-assets/scripts/host.js
+++ static-assets/scripts/host.js
@@ -567,10 +567,8 @@
 
     path = path.replace('//', '/');
 
-    console.log(path);
     CStudioAuthoring.Service.lookupContentItem(CStudioAuthoringContext.site, path, {
       success: function (content) {
-        console.log('soy yo');
         CStudioAuthoring.SelectedContent.setContent(content.item);
         selectContentSet(content.item, false);
       }
@@ -625,7 +623,6 @@
   function selectStudioContent(site, url) {
     CStudioAuthoring.Service.lookupContentItem(site, url, {
       success: function (content) {
-        console.log('maybe yo');
         if (content.item.isPage) {
           CStudioAuthoring.SelectedContent.setContent(content.item);
           selectContentSet(content.item, true);
