c26503fee9dd0d7f3bd9b431141462846338d886
jvega190 <jose.vega@craftersoftware.com>
2020-02-25 16:52:13 -0600

    fromTopic return type

--- ui/guest/src/communicator.ts
+++ ui/guest/src/communicator.ts
@@ -15,8 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { fromEvent } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
 import { filter, map, share } from 'rxjs/operators';
+import { ContentType } from './models/ContentType';
 
 const useBroadcastChannel = (
   (window.parent === window) &&
@@ -33,7 +34,7 @@ export const post = useBroadcastChannel
   ? (type, payload?) => broadcastChannel.postMessage((typeof type === 'object') ? type : { type, payload })
   : (type, payload?) => window.parent.postMessage((typeof type === 'object') ? type : { type, payload }, '*');
 
-export function fromTopic(type: string) {
+export function fromTopic(type: string): Observable<{ type: string, payload: ContentType[] }> {
   return message$.pipe(
     filter((e: MessageEvent) => e.data?.type === type),
     map(e => e.data)
