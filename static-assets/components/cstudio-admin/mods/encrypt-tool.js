3b0ac5e1ce2bad2047765aee8dbdfcad85bce313
Dejan Brkic <dejan.brkic@craftersoftware.com>
2020-02-26 21:41:24 +0100

    added new audit action

---
+++ static-assets/components/cstudio-admin/mods/encrypt-tool.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2007-2019 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+(function () {
+  var EncryptTool =
+  /*#__PURE__*/
+  function () {
+    function EncryptTool(config, el) {
+      CrafterCMSNext.util.babel.classCallCheck(this, EncryptTool);
+      this.containerEl = el;
+      this.config = config;
+      this.types = [];
+    }
+
+    CrafterCMSNext.util.babel.createClass(EncryptTool, [{
+      key: "initialize",
+      value: function initialize(config) {
+        this.config = config;
+      }
+    }, {
+      key: "renderWorkarea",
+      value: function renderWorkarea() {
+        var workarea = document.querySelector('#cstudio-admin-console-workarea');
+        var el = document.createElement('div');
+        $(workarea).html('');
+        workarea.appendChild(el);
+        CrafterCMSNext.render(el, 'EncryptTool');
+      }
+    }]);
+    return EncryptTool;
+  }();
+
+  CStudioAuthoring.Module.moduleLoaded('cstudio-console-tools-encrypt-tool', EncryptTool);
+})();
