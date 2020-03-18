fe8ef7910a0fccd052d5b33d13fe52ba644d0273
hungtsou <hungtsougg@gmail.com>
2020-03-17 19:39:47 -0600

    Add quickCreate menu styles and title, set siteId from query params, add react-router types package

--- ui/app/src/modules/Preview/QuickCreate.tsx
+++ ui/app/src/modules/Preview/QuickCreate.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
+import { Link, useLocation } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import AddCircleIcon from '@material-ui/icons/AddCircleRounded';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { palette } from "../../styles/theme";
 import { getQuickCreateContentList } from '../../services/content';
+
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,12 +23,42 @@ const useStyles = makeStyles((theme: Theme) =>
       "&:hover": {
         fill: palette.green.shade
       }
+    },
+
+    menu: {
+      "& ul": {
+        paddingTop: 0
+      }
+    },
+
+    menuTitle: {
+      fontSize: 10,
+      fontWeight: 'bold',
+      backgroundColor: palette.gray.light2,
+      borderBottom: palette.gray.light3,
+      marginBottom: 5,
+      textTransform: 'uppercase',
+
+      "&:hover": {
+        backgroundColor: palette.gray.light2
+      }
+    },
+
+    menuItem: {
+      fontSize: 12,
+      
+      "& a": {
+        color: palette.black,
+        textDecoration: 'none'
+      } 
     }
   })
 );
 
 export default function QuickCreate() {
   const classes = useStyles({});
+  const query = useQuery();
+  const siteId = query.get('site');
   const [anchorEl, setAnchorEl] = useState(null);
   const [quickCreateContentList, setQuickCreateContentList] = useState([]);
 
@@ -31,15 +67,17 @@ export default function QuickCreate() {
   const handleClose = () => setAnchorEl(null);
 
   useEffect(() => {
-    getQuickCreateContentList('editorial').subscribe(data => 
+    getQuickCreateContentList(siteId).subscribe(data => 
       setQuickCreateContentList(data.items)
     )
-  }, [])
+  }, [siteId])
+
 
   return (
     <>
       <AddCircleIcon fontSize="small" onClick={handleClick} className={classes.addBtn}/>
       <Menu
+        className={classes.menu}
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
@@ -48,9 +86,17 @@ export default function QuickCreate() {
         anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
         transformOrigin={{ vertical: 'top', horizontal: 'left' }}
       >
+        <MenuItem className={classes.menuTitle}>
+          <FormattedMessage
+            id="quickCreateMenu.title"
+            defaultMessage="quick create"
+          />
+        </MenuItem>
         
         { quickCreateContentList.length && quickCreateContentList.map((item, i) => (
-          <MenuItem key={i} onClick={handleClose}><Link to='/'>{item.label}</Link></MenuItem>
+          <MenuItem key={i} onClick={handleClose} className={classes.menuItem}>
+            <Link to='/'>{item.label}</Link>
+          </MenuItem>
         )) }
 
       </Menu>
