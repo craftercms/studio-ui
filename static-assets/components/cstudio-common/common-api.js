76d6433aa43453ac9b6c67922608c9ee14da15d7
jvega190 <jose.vega@craftersoftware.com>
2020-04-08 10:32:13 -0600

    Revert common-api and add DependenciesDialog integration

--- static-assets/components/cstudio-common/common-api.js
+++ static-assets/components/cstudio-common/common-api.js
@@ -470,13 +470,13 @@ var nodeOpen = false,
      * authoring module manager
      */
     'Module': {
-      loadedModules: [],
-      waitingForModule: [],
+      loadedModules: new Array(),
+      waitingForModule: new Array(),
 
       /**
        * either receive the Module Class or wait for it to be loaded
        */
-      requireModule: function (moduleName, script, moduleConfig, callback) {
+      requireModule: function(moduleName, script, moduleConfig, callback) {
         var moduleClass = this.loadedModules[moduleName];
 
         if (!moduleClass) {
@@ -869,7 +869,7 @@ var nodeOpen = false,
         }).then((done) => (unmount = done.unmount));
       },
 
-      viewDependencies: function (site, items, approveType, defaultSelection) {
+      viewDependencies: function(site, items, approveType, defaultSelection) {
         const container = ($('<div class="delete-dialog-container"/>').appendTo('body'))[0];
         //defaultSelection may be: 'depends-on' (default) or 'depends-on-me',
         const dependenciesShown = defaultSelection ? defaultSelection : 'depends-on';
@@ -2153,7 +2153,7 @@ var nodeOpen = false,
             //Remove current content type from list.
             var originalTypesCount = contentTypes.length;
             if (currentContentType && contentTypes.length > 1) {
-              var newContentTypes = [];
+              var newContentTypes = new Array();
               for (var typeIdx = 0; typeIdx < contentTypes.length; typeIdx++) {
                 var contType = contentTypes[typeIdx];
                 if (contType.form != currentContentType) {
@@ -2634,7 +2634,7 @@ var nodeOpen = false,
                                 filePath,
                                 false,
                                 editCb,
-                                []
+                                new Array()
                               );
                             } else {
                               refreshFn(parentItemTo.item, null);
@@ -4754,7 +4754,7 @@ var nodeOpen = false,
         return map;
       },
       getChildren: function(parentItem, flatMap) {
-        var children = [];
+        var children = new Array();
         for (var key in flatMap) {
           var aItem = flatMap[key];
           if (aItem.mandatoryParent == parentItem.uri) {
@@ -5249,21 +5249,21 @@ var nodeOpen = false,
           modules;
 
         if (!groups.length) {
-          groups = [];
+          groups = new Array();
           groups[0] = dropdownConfig.groups.group;
         }
 
         for (var i = 0, a = groups.length; i < a; i++) {
           menuItems = groups[i].menuItems;
           if (!menuItems.length) {
-            menuItems = [];
+            menuItems = new Array();
             menuItems[0] = groups[i].menuItems.menuItem;
           }
 
           for (j = 0, b = menuItems.length; j < b; j++) {
             modules = menuItems[j].modulehooks;
             if (!modules.length) {
-              modules = [];
+              modules = new Array();
               modules[0] = menuItems[j].modulehooks.moduleHook;
             }
 
@@ -6492,7 +6492,7 @@ var nodeOpen = false,
         while (el.hasChildNodes()) {
           el.removeChild(el.firstChild);
         }
-
+        return;
       },
 
       /**
@@ -6511,7 +6511,7 @@ var nodeOpen = false,
             }
           }
         }
-
+        return;
       },
 
       /**
@@ -7287,8 +7287,8 @@ var nodeOpen = false,
           // p.m.
           {
             re: /(\d{1,2}):(\d{1,2}):(\d{1,2})(?:p| p)/,
-            example: ['9:55:00 pm', '12:55:00 p.m.', '9:55:00 p', '11:5:10pm', '9:5:1p'],
-            handler: function (bits) {
+            example: new Array('9:55:00 pm', '12:55:00 p.m.', '9:55:00 p', '11:5:10pm', '9:5:1p'),
+            handler: function(bits) {
               var d = new Date();
               var h = parseInt(bits[1], 10);
               d.setHours(h);
@@ -7300,8 +7300,8 @@ var nodeOpen = false,
           // p.m., no seconds
           {
             re: /(\d{1,2}):(\d{1,2})(?:p| p)/,
-            example: ['9:55 pm', '12:55 p.m.', '9:55 p', '11:5pm', '9:5p'],
-            handler: function (bits) {
+            example: new Array('9:55 pm', '12:55 p.m.', '9:55 p', '11:5pm', '9:5p'),
+            handler: function(bits) {
               var d = new Date();
               var h = parseInt(bits[1], 10);
               d.setHours(h);
@@ -7313,8 +7313,8 @@ var nodeOpen = false,
           // p.m., hour only
           {
             re: /(\d{1,2})(?:p| p)/,
-            example: ['9 pm', '12 p.m.', '9 p', '11pm', '9p'],
-            handler: function (bits) {
+            example: new Array('9 pm', '12 p.m.', '9 p', '11pm', '9p'),
+            handler: function(bits) {
               var d = new Date();
               var h = parseInt(bits[1], 10);
               d.setHours(h);
@@ -7326,8 +7326,8 @@ var nodeOpen = false,
           // hh:mm:ss
           {
             re: /(\d{1,2}):(\d{1,2}):(\d{1,2})/,
-            example: ['9:55:00', '19:55:00', '19:5:10', '9:5:1', '9:55:00 a.m.', '11:55:00a'],
-            handler: function (bits) {
+            example: new Array('9:55:00', '19:55:00', '19:5:10', '9:5:1', '9:55:00 a.m.', '11:55:00a'),
+            handler: function(bits) {
               var d = new Date();
               var h = parseInt(bits[1], 10);
               if (h == 12) {
@@ -7342,8 +7342,8 @@ var nodeOpen = false,
           // hh:mm
           {
             re: /(\d{1,2}):(\d{1,2})/,
-            example: ['9:55', '19:55', '19:5', '9:55 a.m.', '11:55a'],
-            handler: function (bits) {
+            example: new Array('9:55', '19:55', '19:5', '9:55 a.m.', '11:55a'),
+            handler: function(bits) {
               var d = new Date();
               var h = parseInt(bits[1], 10);
               if (h == 12) {
@@ -7358,8 +7358,8 @@ var nodeOpen = false,
           // hhmmss
           {
             re: /(\d{1,6})/,
-            example: ['9', '9a', '9am', '19', '1950', '195510', '0955'],
-            handler: function (bits) {
+            example: new Array('9', '9a', '9am', '19', '1950', '195510', '0955'),
+            handler: function(bits) {
               var d = new Date();
               var h = bits[1].substring(0, 2);
               var m = parseInt(bits[1].substring(2, 4), 10);
@@ -7410,7 +7410,7 @@ var nodeOpen = false,
                         isShiftPlusTabPressed = false;
                       }
                     }
-
+                    return;
                   },
                   isDefault: false
                 }
@@ -7448,7 +7448,7 @@ var nodeOpen = false,
                             isShiftPlusTabPressed = false;
                           }
                         }
-
+                        return;
                       },
                       isDefault: false
                     }
@@ -7727,7 +7727,7 @@ var nodeOpen = false,
        * for a given tree node look up the last item
        */
       getContentItemStatus: function(contentTO, navbarStatus) {
-        var status = {};
+        var status = new Object();
         status.string = '';
         status.key = '';
 
@@ -9081,7 +9081,7 @@ var nodeOpen = false,
        */
       openChildSearch: function(childSearchConfig) {
         if (this.searches == null) {
-          this.searches = [];
+          this.searches = new Array();
         }
 
         this.searches[childSearchConfig.searchId] = childSearchConfig;
