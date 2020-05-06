cbf6c787fbab58b04bced6f5937c33052ed2350b
rart <royart@me.com>
2020-05-06 13:56:08 +0200

    Improve firefox experience by removing capture groups from the individual reg exps so they run, still mapping them to their keys so UI can show individual check marks per named condition

--- ui/app/src/components/SystemStatus/LoginView.tsx
+++ ui/app/src/components/SystemStatus/LoginView.tsx
@@ -556,11 +556,9 @@ function LanguageDropDown(props: LanguageDropDownProps) {
 function getPrimeMatter(props: Partial<PasswordRequirementsDisplayProps>) {
   const { passwordRequirementsRegex, formatMessage } = props;
   let regEx = null;
-  let captureGroups;
+  let captureGroups = passwordRequirementsRegex.match(/\(\?<.*?>.*?\)/g);
   let fallback;
-  try {
-    regEx = new RegExp(passwordRequirementsRegex);
-    captureGroups = passwordRequirementsRegex.match(/\(\?<.*?>.*?\)/g);
+  let namedCaptureGroupSupport = false;
     if (!captureGroups) {
       // RegExp may be valid and have no capture groups
       fallback = {
@@ -568,14 +566,17 @@ function getPrimeMatter(props: Partial<PasswordRequirementsDisplayProps>) {
         description: formatMessage(passwordRequirementMessages.validationPassing)
       };
     }
+  try {
+    regEx = new RegExp(passwordRequirementsRegex);
+    captureGroups = passwordRequirementsRegex.match(/\(\?<.*?>.*?\)/g);
+    namedCaptureGroupSupport = true;
   } catch (error) {
     console.warn(error);
     try {
+      // If the reg ex is parsable without the capture groups, we can use the
+      // reg ex without the capture groups and just need to remove the capture
+      // group from the individual pieces later on the mapping.
       regEx = new RegExp(passwordRequirementsRegex.replace(/\?<(.*?)>/g, ''));
-      fallback = {
-        regEx,
-        description: formatMessage(passwordRequirementMessages.validationPassing)
-      };
     } catch (error) {
       // Allow everything and default to backend as regex wasn't
       // parsable/valid for current navigator
@@ -593,6 +594,9 @@ function getPrimeMatter(props: Partial<PasswordRequirementsDisplayProps>) {
       let description;
       let captureGroupKey =
         captureGroup.match(/\?<(.*?)>/g)?.[0].replace(/\?<|>/g, '') ?? 'Unnamed condition';
+      if (!namedCaptureGroupSupport) {
+        captureGroup = captureGroup.replace(/\?<(.*?)>/g, '');
+      }
       switch (captureGroupKey) {
         case 'hasSpecialChars':
           const allowedChars = (passwordRequirementsRegex.match(
