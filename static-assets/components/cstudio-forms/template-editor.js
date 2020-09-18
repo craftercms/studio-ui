f0e1a258370de72020d3056d6fbd5065b34da125
Aaron <aronchj7@live.com>
2020-09-18 09:44:11 -0600

    Fixing issue when site does not have locales

--- static-assets/components/cstudio-forms/template-editor.js
+++ static-assets/components/cstudio-forms/template-editor.js
@@ -757,6 +757,7 @@ CStudioAuthoring.Module.requireModule(
               addLocales: (headerEl, aceEditor, templatePath, filename, defaultContent) => {
                 CrafterCMSNext.services.translation.getSiteLocales(CStudioAuthoringContext.site).subscribe(
                   ({ localeCodes }) => {
+                    if (localesCodes) return;
                     const $select = $('<select id="locale-selector" class="template-editor-locales-selector"></select>');
                     const options = [];
                     let defaultLocale = '';
