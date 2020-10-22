b8d2be4c62e59d138c518f2a4a154272df463850
Aaron <aronchj7@live.com>
2020-10-22 11:19:50 -0600

    adding hover states;

--- ui/app/src/modules/Preview/ResizeableDrawer.tsx
+++ ui/app/src/modules/Preview/ResizeableDrawer.tsx
@@ -16,9 +16,10 @@
 
 import { DrawerProps } from '@material-ui/core';
 import Drawer from '@material-ui/core/Drawer';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
+import palette from '../../styles/palette';
 
 const useStyles = makeStyles((theme) => ({
   drawer: {
@@ -29,10 +30,10 @@ const useStyles = makeStyles((theme) => ({
     bottom: 0,
     height: 'auto',
     zIndex: theme.zIndex.appBar - 1,
-    paddingRight: '5px'
+    borderRight: 0
   },
   dragger: {
-    width: '5px',
+    width: '1px',
     cursor: 'ew-resize',
     padding: '4px 0 0',
     borderTop: '1px solid #ddd',
@@ -41,7 +42,13 @@ const useStyles = makeStyles((theme) => ({
     right: 0,
     bottom: 0,
     zIndex: 100,
-    backgroundColor: '#f4f7f9'
+    backgroundColor: 'rgba(0, 0, 0, 0.12)',
+    transition: 'width 200ms',
+    '&.hovered, &:hover': {
+      width: '4px',
+      visibility: 'visible',
+      backgroundColor: palette.blue.tint
+    }
   }
 }));
 
@@ -53,6 +60,7 @@ interface ResizeableDrawerProps extends DrawerProps {
 
 export default function(props: ResizeableDrawerProps) {
   const classes = useStyles();
+  const [hovered, setHovered] = useState(false);
   const {
     open,
     children,
@@ -68,13 +76,15 @@ export default function(props: ResizeableDrawerProps) {
     (e) => {
       e.preventDefault();
       const newWidth = e.clientX - document.body.offsetLeft;
-      onWidthChange(newWidth);
+      onWidthChange(newWidth+5);
     },
     [onWidthChange]
   );
 
   const handleMouseDown = () => {
+    setHovered(true);
     const handleMouseUp = () => {
+      setHovered(false);
       document.removeEventListener('mouseup', handleMouseUp, true);
       document.removeEventListener('mousemove', handleMouseMove, true);
     };
@@ -92,7 +102,7 @@ export default function(props: ResizeableDrawerProps) {
       PaperProps={{ ...PaperProps, style: { width } }}
       {...rest}
     >
-      <div onMouseDown={handleMouseDown} className={classes.dragger} />
+      <div onMouseDown={handleMouseDown} className={ clsx(classes.dragger, hovered && 'hovered')} />
       {children}
     </Drawer>
   );
