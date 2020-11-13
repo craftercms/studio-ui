c6cccf58f6f822c5b36ee3357c6a02a57f6bd672
Aaron <aronchj7@live.com>
2020-11-13 10:47:13 -0600

    moving SnackbarCloseButton.tsx to it own file;

---
+++ ui/app/src/components/SnackbarCloseButton/SnackbarCloseButton.tsx
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2007-2020 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { useSnackbar } from 'notistack';
+import { IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import React from 'react';
+
+export default function SnackbarCloseButton({ id }) {
+  const { closeSnackbar } = useSnackbar();
+
+  return (
+    <IconButton onClick={() => closeSnackbar(id)} size="small" color="secondary">
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+}
