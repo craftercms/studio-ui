d3f22a6670511f461ba5494211cc0260956d702c
rart <royart@me.com>
2020-12-10 09:40:08 +0100

    Ability to set confirm dialog styles through state/actions

--- ui/app/src/components/SystemStatus/GlobalDialogManager.tsx
+++ ui/app/src/components/SystemStatus/GlobalDialogManager.tsx
@@ -132,6 +132,7 @@ function GlobalDialogManager() {
         open={state.confirm.open}
         title={state.confirm.title}
         body={state.confirm.body}
+        styles={state.confirm.styles}
         hideBackdrop={state.confirm.hideBackdrop}
         onOk={createCallback(state.confirm.onOk, dispatch)}
         onCancel={createCallback(state.confirm.onCancel, dispatch)}
