dfffac109e3753c14ee67cce12139c7027af6088
Aaron <aronchj7@live.com>
2020-12-11 12:09:00 -0600

    reverting change;

--- ui/guest/src/index.tsx
+++ ui/guest/src/index.tsx
@@ -42,10 +42,10 @@ export {
   useGuestContext
 };
 
-export function initPageBuilder({ path, documentDomain }) {
+export function initPageBuilder({ path }) {
   const guestProxyElement = document.createElement('craftercms-guest-proxy');
   ReactDOM.render(
-    <Guest path={path} isAuthoring documentDomain={documentDomain}>
+    <Guest path={path} isAuthoring>
       <GuestProxy />
     </Guest>,
     guestProxyElement
