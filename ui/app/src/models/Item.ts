28d6e4f47be47e06a073cea5e9e5d61b1ec6c9d5
Aaron <aronchj7@live.com>
2021-02-01 15:01:02 -0600

    adding SystemType to BaseItem;

--- ui/app/src/models/Item.ts
+++ ui/app/src/models/Item.ts
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { SystemType } from './SystemType';
+
 type States =
   | 'new'
   | 'modified'
@@ -35,7 +37,7 @@ export interface BaseItem {
   contentTypeId: string;
   path: string;
   previewUrl: string;
-  systemType: string;
+  systemType: SystemType;
   mimeType: string;
   state: number;
   stateMap: { [key in States]?: boolean };
