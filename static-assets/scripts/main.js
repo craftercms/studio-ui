75833cc2018c2aec8481734f11d52f6cc497e618
jvega190 <jose.vega@craftersoftware.com>
2021-02-02 17:58:17 -0600

    Update after merge with develop

--- static-assets/scripts/main.js
+++ static-assets/scripts/main.js
@@ -1387,28 +1387,43 @@
       $scope.save = function() {
         enableUI(false);
         const value = aceEditor.getValue();
-        configurationApi
-          .writeConfiguration('studio_root', '/configuration/studio-config-override.yaml', 'studio', value)
-          .subscribe(
-            () => {
-              enableUI(true);
-              defaultValue = value;
-              aceEditor.focus();
-              globalConfig.isModified = false;
-              $element.notify(formatMessage(globalConfigMessages.successfulSave), {
-                position: 'top left',
-                className: 'success'
-              });
-              $scope.$apply();
-            },
-            () => {
-              $element.notify(formatMessage(globalConfigMessages.failedSave), {
-                position: 'top left',
-                className: 'error'
-              });
-              $scope.$apply();
+        const errors = fileErrors(aceEditor);
+
+        if (errors.length) {
+          CrafterCMSNext.system.store.dispatch({
+            type: 'SHOW_SYSTEM_NOTIFICATION',
+            payload: {
+              message: formatMessage(globalConfigMessages.documentError),
+              options: {
+                variant: 'error'
+              }
             }
-          );
+          });
+          enableUI(true);
+        } else {
+          configurationApi
+            .writeConfiguration('studio_root', '/configuration/studio-config-override.yaml', 'studio', value)
+            .subscribe(
+              () => {
+                enableUI(true);
+                defaultValue = value;
+                aceEditor.focus();
+                globalConfig.isModified = false;
+                $element.notify(formatMessage(globalConfigMessages.successfulSave), {
+                  position: 'top left',
+                  className: 'success'
+                });
+                $scope.$apply();
+              },
+              () => {
+                $element.notify(formatMessage(globalConfigMessages.failedSave), {
+                  position: 'top left',
+                  className: 'error'
+                });
+                $scope.$apply();
+              }
+            );
+        }
       };
 
       $scope.reset = function() {
