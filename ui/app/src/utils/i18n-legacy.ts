54b759375b70724228144243466dcaa5e9b6c8a6
jvega190 <josevega19@gmail.com>
2021-04-15 16:50:48 -0600

    Add notes in Publishing screens - #4650

--- ui/app/src/utils/i18n-legacy.ts
+++ ui/app/src/utils/i18n-legacy.ts
@@ -583,6 +583,15 @@ const publishingMessages = defineMessages({
   unlockFailed: {
     id: 'unlockPublisherDialog.unlockFailedMessage',
     defaultMessage: 'Error releasing publisher lock.'
+  },
+  bulkPublishNote: {
+    id: 'publishing.bulkPublishNote',
+    defaultMessage: 'Bulk publish should be used to publish changes made in Studio via the UI.'
+  },
+  publishByNote: {
+    id: 'publishing.publishByNote',
+    defaultMessage:
+      'Publish by commit ID(s) must be used for changes made via direct git actions against the repository or pulled from a remote repository.'
   }
 });
 
