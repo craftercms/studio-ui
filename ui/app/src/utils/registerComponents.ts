3852895fa762c1837506a55c726167775f76a15c
jvega190 <josevega19@gmail.com>
2021-06-21 09:07:06 -0600

    Change registerComponents to use lazy - #4788

--- ui/app/src/utils/registerComponents.ts
+++ ui/app/src/utils/registerComponents.ts
@@ -14,109 +14,68 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import SearchIcon from '@material-ui/icons/SearchRounded';
-import ExtensionOutlinedIcon from '@material-ui/icons/ExtensionOutlined';
-import Asset from '@material-ui/icons/ImageOutlined';
-import Audiences from '@material-ui/icons/EmojiPeopleRounded';
-import Simulator from '@material-ui/icons/DevicesRounded';
-import BrushOutlinedIcon from '@material-ui/icons/BrushOutlined';
-import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
-import DashboardRoundedIcon from '@material-ui/icons/DashboardRounded';
-import TuneRoundedIcon from '@material-ui/icons/TuneRounded';
-import DescriptionOutlined from '@material-ui/icons/DescriptionOutlined';
-import DescriptionRounded from '@material-ui/icons/DescriptionRounded';
-import ExtensionRoundedIcon from '@material-ui/icons/ExtensionRounded';
-import LocalOfferOutlinedIcon from '@material-ui/icons/LocalOfferOutlined';
-import LocalOfferRoundedIcon from '@material-ui/icons/LocalOfferRounded';
-import ImageRoundedIcon from '@material-ui/icons/ImageRounded';
-import CodeRoundedIcon from '@material-ui/icons/CodeRounded';
-import InsertDriveFileRoundedIcon from '@material-ui/icons/InsertDriveFileRounded';
-import InsertDriveFileOutlinedIcon from '@material-ui/icons/InsertDriveFileOutlined';
-import WidgetsOutlinedIcon from '@material-ui/icons/WidgetsOutlined';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import SettingsApplicationsOutlinedIcon from '@material-ui/icons/SettingsApplicationsOutlined';
-import FormatAlignJustifyRoundedIcon from '@material-ui/icons/FormatAlignJustifyRounded';
-import FormatAlignLeftRoundedIcon from '@material-ui/icons/FormatAlignLeftRounded';
-import CloudUploadOutlinedIcon from '@material-ui/icons/CloudUploadOutlined';
-import PollOutlinedIcon from '@material-ui/icons/PollOutlined';
-import PageExplorer from '../components/Icons/PageExplorerRounded';
-import SiteExplorer from '../components/Icons/SiteExplorerRounded';
-import Component from '../components/Icons/Component';
-import GraphQL from '../components/Icons/GraphQL';
 import { components } from '../services/plugin';
-import LauncherLinkTile from '../components/LauncherLinkTile';
-import LauncherPublishingStatusTile from '../components/LauncherPublishingStatusTile';
-import LauncherSection from '../components/LauncherSection';
-import LauncherGlobalNav from '../components/LauncherGlobalNav';
-import PreviewIcon from '../components/Icons/Preview';
-import About from '../components/Icons/About';
-import Docs from '../components/Icons/Docs';
-import SitesRounded from '../components/Icons/SitesRounded';
-import BuildIcon from '@material-ui/icons/BuildRounded';
-import AccountCircleRounded from '@material-ui/icons/AccountCircleRounded';
-import PeopleRounded from '@material-ui/icons/PeopleRounded';
-import SupervisedUserCircleRounded from '@material-ui/icons/SupervisedUserCircleRounded';
-import StorageRounded from '@material-ui/icons/StorageRounded';
-import SubjectRounded from '@material-ui/icons/SubjectRounded';
-import SettingsApplicationsRounded from '@material-ui/icons/SettingsApplicationsRounded';
-import FormatAlignCenterRounded from '@material-ui/icons/FormatAlignCenterRounded';
-import LockRounded from '@material-ui/icons/LockRounded';
-import VpnKeyRounded from '@material-ui/icons/VpnKeyRounded';
-import PublicRounded from '@material-ui/icons/PublicRounded';
-import VideocamOutlinedIcon from '@material-ui/icons/VideocamOutlined';
 import { lazy } from 'react';
 
 export const registerComponents = () => {
   Object.entries({
-    '@material-ui/icons/SearchRounded': SearchIcon,
-    '@material-ui/icons/ExtensionOutlined': ExtensionOutlinedIcon,
-    '@material-ui/icons/ImageOutlined': Asset,
-    '@material-ui/icons/EmojiPeopleRounded': Audiences,
-    '@material-ui/icons/DevicesRounded': Simulator,
-    '@material-ui/icons/BrushOutlined': BrushOutlinedIcon,
-    '@material-ui/icons/SettingsOutlined': SettingsOutlinedIcon,
-    '@material-ui/icons/DashboardRounded': DashboardRoundedIcon,
-    '@material-ui/icons/TuneRounded': TuneRoundedIcon,
-    '@material-ui/icons/DescriptionOutlined': DescriptionOutlined,
-    '@material-ui/icons/DescriptionRounded': DescriptionRounded,
-    '@material-ui/icons/ExtensionRounded': ExtensionRoundedIcon,
-    '@material-ui/icons/LocalOfferOutlined': LocalOfferOutlinedIcon,
-    '@material-ui/icons/LocalOfferRounded': LocalOfferRoundedIcon,
-    '@material-ui/icons/ImageRounded': ImageRoundedIcon,
-    '@material-ui/icons/CodeRounded': CodeRoundedIcon,
-    '@material-ui/icons/InsertDriveFileRounded': InsertDriveFileRoundedIcon,
-    '@material-ui/icons/InsertDriveFileOutlined': InsertDriveFileOutlinedIcon,
-    '@material-ui/icons/WidgetsOutlined': WidgetsOutlinedIcon,
-    '@material-ui/icons/LockOutlined': LockOutlinedIcon,
-    '@material-ui/icons/SettingsApplicationsOutlined': SettingsApplicationsOutlinedIcon,
-    '@material-ui/icons/FormatAlignJustifyRounded': FormatAlignJustifyRoundedIcon,
-    '@material-ui/icons/FormatAlignLeftRounded': FormatAlignLeftRoundedIcon,
-    '@material-ui/icons/CloudUploadOutlined': CloudUploadOutlinedIcon,
-    '@material-ui/icons/PollOutlined': PollOutlinedIcon,
-    'craftercms.icons.PageExplorer': PageExplorer,
-    'craftercms.icons.SiteExplorer': SiteExplorer,
-    'craftercms.icons.Component': Component,
-    'craftercms.icons.GraphQL': GraphQL,
-    'craftercms.components.LauncherLinkTile': LauncherLinkTile,
-    'craftercms.components.LauncherPublishingStatusTile': LauncherPublishingStatusTile,
-    'craftercms.components.LauncherSection': LauncherSection,
-    'craftercms.components.LauncherGlobalNav': LauncherGlobalNav,
-    'craftercms.icons.Preview': PreviewIcon,
-    'craftercms.icons.CrafterIcon': About,
-    'craftercms.icons.Docs': Docs,
-    'craftercms.icons.Sites': SitesRounded,
-    '@material-ui/icons/BuildRounded': BuildIcon,
-    '@material-ui/icons/AccountCircleRounded': AccountCircleRounded,
-    '@material-ui/icons/PeopleRounded': PeopleRounded,
-    '@material-ui/icons/SupervisedUserCircleRounded': SupervisedUserCircleRounded,
-    '@material-ui/icons/StorageRounded': StorageRounded,
-    '@material-ui/icons/SubjectRounded': SubjectRounded,
-    '@material-ui/icons/SettingsApplicationsRounded': SettingsApplicationsRounded,
-    '@material-ui/icons/FormatAlignCenterRounded': FormatAlignCenterRounded,
-    '@material-ui/icons/LockRounded': LockRounded,
-    '@material-ui/icons/VpnKeyRounded': VpnKeyRounded,
-    '@material-ui/icons/PublicRounded': PublicRounded,
-    '@material-ui/icons/VideocamOutlined': VideocamOutlinedIcon,
+    '@material-ui/icons/SearchRounded': lazy(() => import('@material-ui/icons/SearchRounded')),
+    '@material-ui/icons/ExtensionOutlined': lazy(() => import('@material-ui/icons/ExtensionOutlined')),
+    '@material-ui/icons/ImageOutlined': lazy(() => import('@material-ui/icons/ImageOutlined')),
+    '@material-ui/icons/EmojiPeopleRounded': lazy(() => import('@material-ui/icons/EmojiPeopleRounded')),
+    '@material-ui/icons/DevicesRounded': lazy(() => import('@material-ui/icons/DevicesRounded')),
+    '@material-ui/icons/BrushOutlined': lazy(() => import('@material-ui/icons/BrushOutlined')),
+    '@material-ui/icons/SettingsOutlined': lazy(() => import('@material-ui/icons/SettingsOutlined')),
+    '@material-ui/icons/DashboardRounded': lazy(() => import('@material-ui/icons/DashboardRounded')),
+    '@material-ui/icons/TuneRounded': lazy(() => import('@material-ui/icons/TuneRounded')),
+    '@material-ui/icons/DescriptionOutlined': lazy(() => import('@material-ui/icons/DescriptionOutlined')),
+    '@material-ui/icons/DescriptionRounded': lazy(() => import('@material-ui/icons/DescriptionRounded')),
+    '@material-ui/icons/ExtensionRounded': lazy(() => import('@material-ui/icons/ExtensionRounded')),
+    '@material-ui/icons/LocalOfferOutlined': lazy(() => import('@material-ui/icons/LocalOfferOutlined')),
+    '@material-ui/icons/LocalOfferRounded': lazy(() => import('@material-ui/icons/LocalOfferRounded')),
+    '@material-ui/icons/ImageRounded': lazy(() => import('@material-ui/icons/ImageRounded')),
+    '@material-ui/icons/CodeRounded': lazy(() => import('@material-ui/icons/CodeRounded')),
+    '@material-ui/icons/InsertDriveFileRounded': lazy(() => import('@material-ui/icons/InsertDriveFileRounded')),
+    '@material-ui/icons/InsertDriveFileOutlined': lazy(() => import('@material-ui/icons/InsertDriveFileOutlined')),
+    '@material-ui/icons/WidgetsOutlined': lazy(() => import('@material-ui/icons/WidgetsOutlined')),
+    '@material-ui/icons/LockOutlined': lazy(() => import('@material-ui/icons/LockOutlined')),
+    '@material-ui/icons/SettingsApplicationsOutlined': lazy(() =>
+      import('@material-ui/icons/SettingsApplicationsOutlined')
+    ),
+    '@material-ui/icons/FormatAlignJustifyRounded': lazy(() => import('@material-ui/icons/FormatAlignJustifyRounded')),
+    '@material-ui/icons/FormatAlignLeftRounded': lazy(() => import('@material-ui/icons/FormatAlignLeftRounded')),
+    '@material-ui/icons/CloudUploadOutlined': lazy(() => import('@material-ui/icons/CloudUploadOutlined')),
+    '@material-ui/icons/PollOutlined': lazy(() => import('@material-ui/icons/PollOutlined')),
+    'craftercms.icons.PageExplorer': lazy(() => import('../components/Icons/PageExplorerRounded')),
+    'craftercms.icons.SiteExplorer': lazy(() => import('../components/Icons/SiteExplorerRounded')),
+    'craftercms.icons.Component': lazy(() => import('../components/Icons/Component')),
+    'craftercms.icons.GraphQL': lazy(() => import('../components/Icons/GraphQL')),
+    'craftercms.components.LauncherLinkTile': lazy(() => import('../components/LauncherLinkTile')),
+    'craftercms.components.LauncherPublishingStatusTile': lazy(() =>
+      import('../components/LauncherPublishingStatusTile')
+    ),
+    'craftercms.components.LauncherSection': lazy(() => import('../components/LauncherSection')),
+    'craftercms.components.LauncherGlobalNav': lazy(() => import('../components/LauncherGlobalNav')),
+    'craftercms.icons.Preview': lazy(() => import('../components/Icons/Preview')),
+    'craftercms.icons.CrafterIcon': lazy(() => import('../components/Icons/About')),
+    'craftercms.icons.Docs': lazy(() => import('../components/Icons/Docs')),
+    'craftercms.icons.Sites': lazy(() => import('../components/Icons/SitesRounded')),
+    '@material-ui/icons/BuildRounded': lazy(() => import('@material-ui/icons/BuildRounded')),
+    '@material-ui/icons/AccountCircleRounded': lazy(() => import('@material-ui/icons/AccountCircleRounded')),
+    '@material-ui/icons/PeopleRounded': lazy(() => import('@material-ui/icons/PeopleRounded')),
+    '@material-ui/icons/SupervisedUserCircleRounded': lazy(() =>
+      import('@material-ui/icons/SupervisedUserCircleRounded')
+    ),
+    '@material-ui/icons/StorageRounded': lazy(() => import('@material-ui/icons/StorageRounded')),
+    '@material-ui/icons/SubjectRounded': lazy(() => import('@material-ui/icons/SubjectRounded')),
+    '@material-ui/icons/SettingsApplicationsRounded': lazy(() =>
+      import('@material-ui/icons/SettingsApplicationsRounded')
+    ),
+    '@material-ui/icons/FormatAlignCenterRounded': lazy(() => import('@material-ui/icons/FormatAlignCenterRounded')),
+    '@material-ui/icons/LockRounded': lazy(() => import('@material-ui/icons/LockRounded')),
+    '@material-ui/icons/VpnKeyRounded': lazy(() => import('@material-ui/icons/VpnKeyRounded')),
+    '@material-ui/icons/PublicRounded': lazy(() => import('@material-ui/icons/PublicRounded')),
+    '@material-ui/icons/VideocamOutlined': lazy(() => import('@material-ui/icons/VideocamOutlined')),
     'craftercms.components.ToolsPanelEmbeddedAppViewButton': lazy(() =>
       import('../components/ToolsPanelEmbeddedAppViewButton')
     ),
