c5b7b4ceddebddef8017807c7ad4d14b6a0261f9
jvega190 <josevega19@gmail.com>
2021-08-03 10:08:43 -0600

    Add uiConfigMessages file - #4886

---
+++ ui/app/src/assets/uiConfigMessages.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2007-2021 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  pageBuilder: {
+    id: 'pageBuilder.title',
+    defaultMessage: 'Page Building'
+  },
+  siteTools: {
+    id: 'siteTools.title',
+    defaultMessage: 'Site Tools'
+  },
+  siteDashboard: {
+    id: 'words.dashboard',
+    defaultMessage: 'Dashboard'
+  },
+  configuration: {
+    id: 'words.configuration',
+    defaultMessage: 'Configuration'
+  },
+  workflowStates: {
+    id: 'workflowStates.title',
+    defaultMessage: 'Workflow States'
+  },
+  publishing: {
+    id: 'words.publishing',
+    defaultMessage: 'Publishing'
+  },
+  remoteRepositories: {
+    id: 'remoteRepositories.title',
+    defaultMessage: 'Remote Repositories'
+  },
+  preview: {
+    id: 'words.preview',
+    defaultMessage: 'Preview'
+  },
+  site: {
+    id: 'launcher.siteSectionTitle',
+    defaultMessage: 'Site <muted>• {siteName}</muted>'
+  }
+});
