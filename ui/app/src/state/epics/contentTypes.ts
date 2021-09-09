d8a1a562bc1845ac9561b19a9c9062ca121d47de
jvega190 <josevega19@gmail.com>
2021-09-09 15:37:30 -0600

    Format contentTypes epics file - #4987

--- ui/app/src/state/epics/contentTypes.ts
+++ ui/app/src/state/epics/contentTypes.ts
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Epic, ofType } from 'redux-observable';
+import { ofType } from 'redux-observable';
 import {
   dissociateTemplateComplete,
   dissociateTemplateFailed,
@@ -28,45 +28,50 @@ import {
 import { exhaustMap, map, switchMap, withLatestFrom } from 'rxjs/operators';
 import { fetchItemsByContentType } from '../../services/content';
 import { catchAjaxError } from '../../utils/ajax';
-import GlobalState from '../../models/GlobalState';
-import { Observable } from 'rxjs';
 import { fetchContentTypes } from '../../services/contentTypes';
 import { dissociateTemplate as dissociateTemplateActionCreator } from '../actions/preview';
 import { dissociateTemplate as dissociateTemplateService } from '../../services/contentTypes';
+import { CrafterCMSEpic } from '../store';
 
-const fetch: Epic = (action$, state$) =>
-  action$.pipe(
-    ofType(FETCH_CONTENT_TYPES),
-    withLatestFrom(state$),
-    exhaustMap(([, { sites: { active: site } }]) =>
-      fetchContentTypes(site).pipe(map(fetchContentTypesComplete), catchAjaxError(fetchContentTypesFailed))
-    )
-  );
-
-const fetchComponentsByContentType: Epic = (action$, state$: Observable<GlobalState>) =>
-  action$.pipe(
-    ofType(FETCH_COMPONENTS_BY_CONTENT_TYPE),
-    withLatestFrom(state$),
-    switchMap(([, state]) =>
-      fetchItemsByContentType(
-        state.sites.active,
-        state.preview.components.contentTypeFilter,
-        state.contentTypes.byId,
-        state.preview.components.query
-      ).pipe(map(fetchComponentsByContentTypeComplete), catchAjaxError(fetchComponentsByContentTypeFailed))
-    )
-  );
-
-const dissociateTemplate: Epic = (action$, state$) =>
-  action$.pipe(
-    ofType(dissociateTemplateActionCreator.type),
-    withLatestFrom(state$),
-    switchMap(([{ payload }, { sites: { active } }]) =>
-      dissociateTemplateService(active, payload.contentTypeId).pipe(
-        map(() => dissociateTemplateComplete({ contentTypeId: payload.contentTypeId })), // TODO: send content type id so it can be updated in reducer
-        catchAjaxError(dissociateTemplateFailed)
+export default [
+  // region fetchContentTypes
+  (action$, state$) =>
+    action$.pipe(
+      ofType(FETCH_CONTENT_TYPES),
+      withLatestFrom(state$),
+      exhaustMap(([, { sites: { active: site } }]) =>
+        fetchContentTypes(site).pipe(map(fetchContentTypesComplete), catchAjaxError(fetchContentTypesFailed))
+      )
+    ),
+  // endregion
+  // region fetchCommponentsByContentType
+  (action$, state$) =>
+    action$.pipe(
+      ofType(FETCH_COMPONENTS_BY_CONTENT_TYPE),
+      withLatestFrom(state$),
+      switchMap(([, state]) =>
+        fetchItemsByContentType(
+          state.sites.active,
+          state.preview.components.contentTypeFilter,
+          state.contentTypes.byId,
+          state.preview.components.query
+        ).pipe(map(fetchComponentsByContentTypeComplete), catchAjaxError(fetchComponentsByContentTypeFailed))
+      )
+    ),
+  // endregion
+  // region fetchCommponentsByContentType
+  (action$, state$) =>
+    action$.pipe(
+      ofType(dissociateTemplateActionCreator.type),
+      withLatestFrom(state$),
+      switchMap(([{ payload }, { sites: { active } }]) =>
+        dissociateTemplateService(active, payload.contentTypeId).pipe(
+          map(() => dissociateTemplateComplete({ contentTypeId: payload.contentTypeId })), // TODO: send content type id so it can be updated in reducer
+          catchAjaxError(dissociateTemplateFailed)
+        )
       )
     )
-  );
+  // endregion dissociateTemplate
+] as CrafterCMSEpic[];
 
-export default [fetch, fetchComponentsByContentType, dissociateTemplate] as Epic[];
+// export default [fetch, fetchComponentsByContentType, dissociateTemplate] as CrafterCMSEpic[];
