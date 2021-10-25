8de13f42e30debe313e3774e70ed2b95c26c716b
jvega190 <josevega19@gmail.com>
2021-10-25 10:28:27 -0600

    Update registerDynamicField to avoid duplicate fields #4808

--- static-assets/components/cstudio-forms/forms-engine.js
+++ static-assets/components/cstudio-forms/forms-engine.js
@@ -570,7 +570,9 @@ var CStudioForms =
 
     CStudioForm.prototype = {
       registerDynamicField: function (name) {
-        this.dynamicFields.push(name);
+        if (!this.dynamicFields.includes(name)) {
+          this.dynamicFields.push(name);
+        }
       },
 
       registerBeforeSaveCallback: function (callback) {
