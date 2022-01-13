449fc3e688667563dcd3551f39e45701c2a72e5a
Roy Art <royart@me.com>
2022-01-13 08:15:05 +0100

    Update static-assets/components/cstudio-common/resources/de/base.js

--- static-assets/components/cstudio-common/resources/de/base.js
+++ static-assets/components/cstudio-common/resources/de/base.js
@@ -850,7 +850,7 @@ CStudioAuthoring.Messages.registerBundle('contentTypes', 'de', {
   enableSearchExisting: "Zeige 'Suchen'",
   useSearch: 'Suche verwenden',
   allowEditWithoutWarning: 'Bearbeiten ohne Warnung zulassen',
-  replaceAccent: 'Convert accent/diacritics to latin characters',
+  replaceAccent: 'Konvertieren Sie Akzente/diakritische Zeichen in lateinische Zeichen',
 
   /*Restrictions*/
   required: 'Erforderlich',
