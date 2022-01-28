7eb80b1cda1f8ea6058c719783f4324e8f13eb5e
rart <royart@me.com>
2022-01-28 11:54:43 +0100

    Update copyrights.

--- static-assets/js/tinymce-plugins/craftercms_paste_extension/craftercms_tinymce_hooks.sample.js
+++ static-assets/js/tinymce-plugins/craftercms_paste_extension/craftercms_tinymce_hooks.sample.js
@@ -2,7 +2,7 @@
 // Should not be actually loaded by the product.
 
 /*
- * Copyright (C) 2007-2021 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
