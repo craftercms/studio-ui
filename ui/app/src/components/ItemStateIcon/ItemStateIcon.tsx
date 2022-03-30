00d4ad0c694f4b6f8ec020e639466e0f7a56ad6f
jvega190 <josevega19@gmail.com>
2022-03-30 10:29:00 -0600

    [5367] Disable the display of Folder states in the UI (#2899)
    * Disable the display of Folder states in the UI #5367
    
    * Update ui/app/src/components/ItemStateIcon/ItemStateIcon.tsx
    
    Co-authored-by: Roy Art <royart@me.com>

--- ui/app/src/components/ItemStateIcon/ItemStateIcon.tsx
+++ ui/app/src/components/ItemStateIcon/ItemStateIcon.tsx
@@ -145,9 +145,16 @@ export function ItemStateIcon(props: ItemStateIconProps) {
       translationPending: null,
       translationInProgress: null
     };
-    return (
-      map[getItemStateId(item.stateMap)] ?? { Icon: NotInWorkflowIcon, stateSpecificClass: classes.stateNotInWorkflow }
-    );
+    if (item.systemType === 'folder') {
+      return { Icon: NotInWorkflowIcon, stateSpecificClass: classes.stateNotInWorkflow };
+    } else {
+      return (
+        map[getItemStateId(item.stateMap)] ?? {
+          Icon: NotInWorkflowIcon,
+          stateSpecificClass: classes.stateNotInWorkflow
+        }
+      );
+    }
   }, [
     classes.stateDeletedIcon,
     classes.stateLockedIcon,
