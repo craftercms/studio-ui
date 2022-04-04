2b74a16ed931165f5c4200997dd17e444501a127
jvega190 <josevega19@gmail.com>
2022-04-04 11:01:39 -0600

    Fix - Not able to view the first item history for configuration files. #5429

--- ui/app/src/state/epics/dialogs.ts
+++ ui/app/src/state/epics/dialogs.ts
@@ -54,7 +54,7 @@ import { changeCurrentUrl, requestWorkflowCancellationDialogOnResult } from '../
 import { CrafterCMSEpic } from '../store';
 import { formEngineMessages } from '../../utils/i18n-legacy';
 import infoGraphic from '../../assets/information.svg';
-import { nou } from '../../utils/object';
+import { nnou, nou } from '../../utils/object';
 import { getHostToGuestBus } from '../../modules/Preview/previewContext';
 
 function getDialogNameFromType(type: string): string {
@@ -199,8 +199,10 @@ const dialogEpics: CrafterCMSEpic[] = [
   (action$, state$) =>
     action$.pipe(
       ofType(showPreviewDialog.type),
-      filter(({ payload }) => payload.type === 'editor'),
       withLatestFrom(state$),
+      filter(
+        ([{ payload }, state]) => payload.type === 'editor' && nnou(payload.url) && nou(state.dialogs.preview.content)
+      ),
       switchMap(([{ payload }, state]) =>
         fetchContentXML(state.sites.active, payload.url).pipe(map((content) => updatePreviewDialog({ content })))
       )
