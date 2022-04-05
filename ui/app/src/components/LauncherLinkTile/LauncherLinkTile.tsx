c549e814005f108fad4603b15686c73e2eabf6eb
rart <royart@me.com>
2022-04-05 18:04:27 +0200

    Revert change and leave TODO for later

--- ui/app/src/components/LauncherLinkTile/LauncherLinkTile.tsx
+++ ui/app/src/components/LauncherLinkTile/LauncherLinkTile.tsx
@@ -47,8 +47,9 @@ const LauncherLinkTile = (props: LauncherLinkTileProps) => {
     ? (e) => {
         if (!e.metaKey) {
           e.preventDefault();
+          // TODO: Re-id craftercms.components.Dashboard => craftercms.components.SiteDashboard when switching to new dashboard
           // prettier-ignore
-          const id = systemLinkId === 'siteDashboardDialog' ? 'craftercms.components.SiteDashboard' : (
+          const id = systemLinkId === 'siteDashboardDialog' ? 'craftercms.components.Dashboard' : (
             systemLinkId === 'siteToolsDialog'
               ? 'craftercms.components.EmbeddedSiteTools'
               : 'craftercms.components.Search'
