cd987a12feb0e1c5d592953565738000623a5284
jvega190 <josevega19@gmail.com>
2022-05-04 10:42:00 -0600

    [5537] CMIS upload dialog is sending the form data incorrectly (#2974)
    * Fix - Buttons are hidden on upload error with CMIS image and file upload data sources #5464
    
    * Fix - CMIS file and image data sources do not allow items to be picked or cloned #5465
    
    * Fix - CMIS upload dialog is sending the form data incorrectly #5537
    
    * Fix - CMIS datasources issues
    
    * Fix - Node selector not showing delete icon for items that are not editable
    
    * Fix - Legacy upload dialogs cancel button dark mode styling issues #5505

--- static-assets/components/cstudio-dialogs/uploadCMIS-dialog.js
+++ static-assets/components/cstudio-dialogs/uploadCMIS-dialog.js
@@ -91,7 +91,6 @@ CStudioAuthoring.Dialogs.UploadCMISDialog = CStudioAuthoring.Dialogs.UploadCMISD
       '<div class="contentTypePopupHeader">Upload</div> ' +
       '<div><form id="asset_upload_form">' +
       '<div class="contentTypeOuter">' +
-      '<div id="uploadContainer"></div>' +
       '<div><table><tr><td><input type="hidden" name="siteId" value="' +
       site +
       '"/></td>' +
@@ -102,6 +101,7 @@ CStudioAuthoring.Dialogs.UploadCMISDialog = CStudioAuthoring.Dialogs.UploadCMISD
       repositoryId +
       '"/></td></tr>' +
       '</table></div>' +
+      '<div id="uploadContainer"></div>' +
       '</div>' +
       '<div class="contentTypePopupBtn"> ' +
       '<input type="button" class="btn btn-default cstudio-xform-button" id="uploadCancelButton" value="Cancel"  />' +
@@ -113,7 +113,7 @@ CStudioAuthoring.Dialogs.UploadCMISDialog = CStudioAuthoring.Dialogs.UploadCMISD
     // Instantiate the Dialog
     upload_dialog = new YAHOO.widget.Dialog('cstudio-wcm-popup-div', {
       width: '410px',
-      height: '255px',
+      'min-height': '255px',
       effect: {
         effect: YAHOO.widget.ContainerEffect.FADE,
         duration: 0.25
@@ -159,8 +159,8 @@ CStudioAuthoring.Dialogs.UploadCMISDialog = CStudioAuthoring.Dialogs.UploadCMISD
         me.uploadingFile = true;
         $('#uploadCancelButton').attr('disabled', true);
       },
-      onComplete: function (result) {
-        let uploaded = result.successful[0].response.body.item;
+      onComplete: function ({ successful }) {
+        let uploaded = JSON.parse(successful[0].response.body.response).item;
 
         $('#uploadCancelButton').attr('disabled', false);
         me.uploadingFile = false;
@@ -168,8 +168,8 @@ CStudioAuthoring.Dialogs.UploadCMISDialog = CStudioAuthoring.Dialogs.UploadCMISD
         me.callback.success(uploaded);
         CStudioAuthoring.Dialogs.UploadCMISDialog.closeDialog();
       },
-      onError: function (file, error, response) {
-        const res = response.body.response,
+      onError: function ({ response }) {
+        const res = JSON.parse(response.body.response).response,
           errorMsg = `${res.message}. ${res.remedialAction}`;
 
         me.uploadingFile = false;
