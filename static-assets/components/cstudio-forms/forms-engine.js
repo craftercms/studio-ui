525ca0349cd0885570ff99291570b39eb6421515
jvega190 <josevega19@gmail.com>
2022-05-23 12:12:40 -0600

    Fix - Cancelling form after save error unlocking wrong path #5586 (#3007)

--- static-assets/components/cstudio-forms/forms-engine.js
+++ static-assets/components/cstudio-forms/forms-engine.js
@@ -1221,15 +1221,17 @@ var CStudioForms =
             }
           }
 
-          var buildEntityIdFn = function (draft) {
+          var buildEntityIdFn = function (draft, useCurrentValidFolder) {
             var entityId = path.replace('.html', '.xml');
             var changeTemplate = CStudioAuthoring.Utils.getQueryVariable(location.search, 'changeTemplate');
             var length = entityId.length;
             var index_html = '';
             var fileName = form.model['file-name'];
             var folderName =
-              form.definition.contentAsFolder || form.definition.contentAsFolder == 'true'
-                ? form.model['folder-name']
+              form.definition.contentAsFolder || form.definition.contentAsFolder === 'true'
+                ? useCurrentValidFolder
+                  ? CStudioForms.currentValidFolder ?? CStudioForms.initialModel['folder-name']
+                  : form.model['folder-name']
                 : undefined;
             /*
              * No folderName means it is NOT a content-as-folder content type.
@@ -1418,6 +1420,7 @@ var CStudioForms =
                 CStudioAuthoring.Service.createServiceUri(serviceUrl),
                 {
                   success: function () {
+                    CStudioForms.currentValidFolder = CStudioForms.updatedModel['folder-name'];
                     YAHOO.util.Event.removeListener(window, 'beforeunload', unloadFn, me);
 
                     var getContentItemCb = {
@@ -1524,6 +1527,9 @@ var CStudioForms =
                     }
                   },
                   failure: function (err) {
+                    if (!CStudioForms.currentValidFolder) {
+                      CStudioForms.currentValidFolder = CStudioForms.initialModel['folder-name'];
+                    }
                     try {
                       CStudioAuthoring.Operations.showSimpleDialog(
                         'error-dialog',
@@ -1716,7 +1722,7 @@ var CStudioForms =
                 unlockBeforeCancel(path);
               } else {
                 if (!form.readOnly && path && path.indexOf('.xml') != -1) {
-                  var entityId = buildEntityIdFn(null);
+                  const entityId = buildEntityIdFn(null, Boolean(CStudioForms.currentValidFolder));
                   CrafterCMSNext.services.content
                     .unlock(CStudioAuthoringContext.site, entityId)
                     .subscribe((response) => {
