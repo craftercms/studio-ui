a0068dc7a33db39e913544c0d283d3b933d485d8
jvega190 <josevega19@gmail.com>
2022-06-04 19:45:08 -0600

    [5615][next] Remove Sandbox Branch from the site creation dialog (#3028)
    * Remove Sandbox Branch from the site creation dialog #5615
    
    * PR review update #5615

--- ui/app/src/components/CreateSiteDialog/BlueprintForm.tsx
+++ ui/app/src/components/CreateSiteDialog/BlueprintForm.tsx
@@ -158,7 +158,10 @@ function BlueprintForm(props: BlueprintFormProps) {
       let parameters = { ...inputs.blueprintFields, [e.target.name]: e.target.value };
       setInputs({ blueprintFields: parameters });
     } else {
-      setInputs({ [e.target.name]: e.target.value });
+      setInputs({
+        [e.target.name]: e.target.value,
+        ...(blueprint.id === 'GIT' && e.target.name === 'repoRemoteBranch' && { sandboxBranch: e.target.value })
+      });
     }
   };
 
@@ -271,18 +274,20 @@ function BlueprintForm(props: BlueprintFormProps) {
             )}
           />
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="sandboxBranch"
-            name="sandboxBranch"
-            label={formatMessage(messages.sandboxBranch)}
-            fullWidth
-            onKeyPress={onKeyPress}
-            onChange={(event) => handleInputChange(event)}
-            placeholder={'master'}
-            value={inputs.sandboxBranch}
-          />
-        </Grid>
+        {blueprint.id !== 'GIT' && (
+          <Grid item xs={12}>
+            <TextField
+              id="sandboxBranch"
+              name="sandboxBranch"
+              label={formatMessage(messages.sandboxBranch)}
+              fullWidth
+              onKeyPress={onKeyPress}
+              onChange={(event) => handleInputChange(event)}
+              placeholder={'master'}
+              value={inputs.sandboxBranch}
+            />
+          </Grid>
+        )}
         <Grid item xs={12}>
           <TextField
             id="description"
