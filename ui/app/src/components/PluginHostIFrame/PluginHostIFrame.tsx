40959150bea60a45359019f353845d2fffd78c16
rart <royart@me.com>
2022-08-01 18:05:21 +0200

    Stability - Fix language setting - Spanish translation updates - Npm package build updates

--- ui/app/src/components/PluginHostIFrame/PluginHostIFrame.tsx
+++ ui/app/src/components/PluginHostIFrame/PluginHostIFrame.tsx
@@ -19,16 +19,21 @@ import { forwardRef, IframeHTMLAttributes } from 'react';
 import useActiveSiteId from '../../hooks/useActiveSiteId';
 import PluginFileBuilder from '../../models/PluginFileBuilder';
 import Box, { BoxProps } from '@mui/material/Box';
+import { toQueryString } from '../../utils/object';
+
+type CompletePluginFileBuilder = Required<PluginFileBuilder>;
+
+type Optional = 'site' | 'id' | 'file';
 
 export interface PluginHostIFrameProps extends IframeHTMLAttributes<HTMLIFrameElement> {
   sx?: BoxProps['sx'];
-  plugin: Pick<PluginFileBuilder, 'type' | 'name' | 'file'>;
+  plugin: Omit<CompletePluginFileBuilder, Optional> & Partial<Pick<CompletePluginFileBuilder, Optional>>;
 }
 
 export const PluginHostIFrame = forwardRef<HTMLIFrameElement, PluginHostIFrameProps>((props, ref) => {
   const {
     sx,
-    plugin: { type, name, file },
+    plugin: { type, name, id, file, site },
     ...other
   } = props;
   const siteId = useActiveSiteId();
@@ -36,10 +41,19 @@ export const PluginHostIFrame = forwardRef<HTMLIFrameElement, PluginHostIFramePr
     <Box
       title="Studio plugin host frame"
       {...other}
-      sx={sx ?? { width: '100%', height: '100%', border: 'none' }}
+      sx={typeof sx === 'object' ? { width: '100%', height: '100%', border: 'none', ...sx } : sx}
       ref={ref}
       component="iframe"
-      src={`/studio/plugin?site=${siteId}&type=${type}&name=${name}${file ? `&file=${file}` : ''}`}
+      src={`/studio/plugin${toQueryString(
+        {
+          site: site || siteId,
+          type,
+          name,
+          file,
+          pluginId: id
+        },
+        { skipNull: true, skipEmptyString: true }
+      )}`}
     />
   );
 });
