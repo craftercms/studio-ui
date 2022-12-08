ac002f364243e82bc1c2686f422c196ba44e349a
jvega190 <josevega19@gmail.com>
2022-12-08 10:46:52 -0600

    Remove maxWidth update in EditGroupDialog, update TransferList to use noWrap in title typography #5878

--- ui/app/src/components/EditGroupDialog/EditGroupDialog.tsx
+++ ui/app/src/components/EditGroupDialog/EditGroupDialog.tsx
@@ -23,7 +23,7 @@ export function EditGroupDialog(props: EditGroupDialogProps) {
   const { group, onGroupDeleted, onGroupSaved, onSubmittingAndOrPendingChange, ...rest } = props;
 
   return (
-    <EnhancedDialog omitHeader maxWidth="lg" {...rest}>
+    <EnhancedDialog omitHeader {...rest}>
       <EditGroupDialogContainer
         group={group}
         onGroupSaved={onGroupSaved}
