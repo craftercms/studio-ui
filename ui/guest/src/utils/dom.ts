21022ad0699f88bd1daa7dde32f2b6c304cd3870
jvega190 <josevega19@gmail.com>
2023-06-19 16:05:08 -0600

    Update handler type #6079

--- ui/guest/src/utils/dom.ts
+++ ui/guest/src/utils/dom.ts
@@ -175,7 +175,6 @@ export function splitRect(rect: DOMRect, axis: string = X_AXIS): DOMRect[] {
   return [rect1, rect2];
 }
 
-// TODO: UPDATE to remove JQuery usage
 export function insertDropMarker({
   $dropMarker,
   insertPosition,
