325feb0ebe60ec6a72fff37f0b1bc8c50aedd20f
jvega190 <josevega19@gmail.com>
2023-07-04 16:07:44 -0600

    Fix incorrect access of model id in ZoneMenu, update iceRegistry model validation #6102

--- ui/guest/src/react/ZoneMenu.tsx
+++ ui/guest/src/react/ZoneMenu.tsx
@@ -130,17 +130,9 @@ export function ZoneMenu(props: ZoneMenuProps) {
     // endregion
     [modelId, recordType, iceRecord]
   );
-  const currentItem = collection?.[elementIndex] ?? null;
   const isItemFile = collection ? Boolean(collection[elementIndex]?.hasOwnProperty('key')) : false;
   const componentId =
-    recordType === 'component'
-      ? modelId
-      : recordType === 'node-selector-item'
-      ? !isItemFile
-        ? // @ts-ignore
-          currentItem.craftercms.id
-        : null
-      : null;
+    recordType === 'component' ? modelId : recordType === 'node-selector-item' ? collection?.[elementIndex] : null;
   const { field, contentType } = useMemo(() => getReferentialEntries(record.iceIds[0]), [record.iceIds]);
   const isMovable =
     ['node-selector-item', 'repeat-item'].includes(recordType) ||
