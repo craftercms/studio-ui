e19d870dbc4cb925a1d31b84e0ac9c2551a4094c
jvega190 <josevega19@gmail.com>
2023-07-14 08:41:26 -0600

    Update compatibilityList #5953

--- ui/app/src/components/PreviewConcierge/compatibilityList.ts
+++ ui/app/src/components/PreviewConcierge/compatibilityList.ts
@@ -14,6 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const list = ['4.0.3', '4.0.4', '4.0.5', '4.0.6', '4.0.7'];
+const list = ['4.0.3', '4.0.7'];
 
 export default list;
