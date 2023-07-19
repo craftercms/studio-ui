479c1ffaec5852d00cec1f019311ee2dc44a2692
jvega190 <josevega19@gmail.com>
2023-07-19 10:23:40 -0600

    PR review updates #6111

--- ui/app/src/utils/content.ts
+++ ui/app/src/utils/content.ts
@@ -417,19 +417,21 @@ export function parseContentXML(
           }
         }
         const field = contentTypesLookup[sourceContentTypeId ?? contentTypeId]?.fields?.[tagName];
-        // date-time control handles the timezone field using {controlName}_tz format. So if the field without `tz` is
-        // found in the content type, we can ignore the `tz` field (and don't log an error).
-        let isTimezoneField = false;
-        if (tagName.endsWith('_tz')) {
-          const withoutTz = tagName.replace(/_tz$/, '');
-          isTimezoneField = Boolean(contentTypesLookup[sourceContentTypeId ?? contentTypeId]?.fields?.[withoutTz]);
-        }
-        if (!field && !isTimezoneField) {
-          console.error(
-            `[parseContentXML] Field "${tagName}" was not found on "${
-              sourceContentTypeId ?? contentTypeId
-            }" content type. "${source ?? path}" may have stale/outdated content properties.`
-          );
+        if (!field) {
+          // date-time control handles the timezone field using {controlName}_tz format. So if the field without `tz` is
+          // found in the content type, we can ignore the `tz` field (and don't log an error).
+          let isTimezoneField = false;
+          if (tagName.endsWith('_tz')) {
+            const withoutTz = tagName.replace(/_tz$/, '');
+            isTimezoneField = Boolean(contentTypesLookup[sourceContentTypeId ?? contentTypeId]?.fields?.[withoutTz]);
+          }
+          if (!isTimezoneField) {
+            console.error(
+              `[parseContentXML] Field "${tagName}" was not found on "${
+                sourceContentTypeId ?? contentTypeId
+              }" content type. "${source ?? path}" may have stale/outdated content properties.`
+            );
+          }
         }
         current[tagName] = parseElementByContentType(element, field, contentTypesLookup, instanceLookup);
       }
