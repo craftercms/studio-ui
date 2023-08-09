ba3873d7637277a8e8fd3978ac38ebddbf31a98e
jvega190 <josevega19@gmail.com>
2023-08-09 14:29:16 -0600

    Replace cp with rsync in npm-content build script 570E

--- ui/npm-content/build.sh
+++ ui/npm-content/build.sh
@@ -20,33 +20,28 @@ libsDirectory=../../static-assets/libs
 
 # ace build
 rm -rf "$libsDirectory/ace"
-cp -r ../../node_modules/ace-builds/src-min-noconflict "$libsDirectory/ace"
+rsync -ar --delete ../../node_modules/ace-builds/src-min-noconflict/ "$libsDirectory/ace/"
 cat src/ace-append.js >> "$libsDirectory/ace/ace.js"
-
 cp src/mode-yaml/* "$libsDirectory/ace"
-
 echo "Ace build complete"
 
 # js-yaml build
 rm -rf "$libsDirectory/js-yaml"
 mkdir "$libsDirectory/js-yaml"
-cp ../../node_modules/js-yaml/dist/js-yaml.min.js "$libsDirectory/js-yaml/js-yaml-4.0.0.min.js"
-
+rsync -ar --delete ../../node_modules/js-yaml/dist/js-yaml.min.js "$libsDirectory/js-yaml/js-yaml-4.0.0.min.js"
 echo "js-yaml build complete"
 
 # bootstrap build
 rm -rf "$libsDirectory/bootstrap"
 mkdir "$libsDirectory/bootstrap"
-cp -r ../../node_modules/bootstrap/scss "$libsDirectory/bootstrap/scss"
-cp ../../node_modules/bootstrap/dist/js/bootstrap.min.js "$libsDirectory/bootstrap/bootstrap.min.js"
-cp ../../node_modules/@popperjs/core/dist/umd/popper.min.js "$libsDirectory/bootstrap/popper.min.js"
-
+rsync -ar --delete ../../node_modules/bootstrap/scss/ "$libsDirectory/bootstrap/scss"
+rsync -ar --delete ../../node_modules/bootstrap/dist/js/bootstrap.min.js "$libsDirectory/bootstrap/bootstrap.min.js"
+rsync -ar --delete ../../node_modules/@popperjs/core/dist/umd/popper.min.js "$libsDirectory/bootstrap/popper.min.js"
 echo "Bootstrap build complete"
 
 # jquery-ui build
 rm -rf "$libsDirectory/jquery-ui"
-cp -r ../../node_modules/jquery-ui/dist "$libsDirectory/jquery-ui"
-cp -r ../../node_modules/jquery-ui-css/jquery-ui.min.css "$libsDirectory/jquery-ui/jquery-ui.min.css"
+rsync -ar --delete ../../node_modules/jquery-ui/dist/ "$libsDirectory/jquery-ui"
+rsync -ar --delete ../../node_modules/jquery-ui-css/jquery-ui.min.css "$libsDirectory/jquery-ui/jquery-ui.min.css"
 sed -i 's/typeof define\&\&define.amd?define/typeof crafterDefine\&\&crafterDefine.amd?crafterDefine/' ../../static-assets/libs/jquery-ui/jquery-ui.min.js
-
 echo "jQuery UI build complete"
