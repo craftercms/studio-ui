edde80503e8c6f3a6dd6632cc82b76a8715dfed6
jvega190 <josevega19@gmail.com>
2023-09-20 14:59:01 -0600

    Remove resolved TODO and only include md={12} in grid item sizes for RepoStatusUI #6211

--- ui/app/src/components/GitManagement/RepoStatus/RepoStatusUI.tsx
+++ ui/app/src/components/GitManagement/RepoStatus/RepoStatusUI.tsx
@@ -96,7 +96,6 @@ export function RepoStatusUI(props: RepoStatusUIProps) {
             <DropDownMenu
               onMenuItemClick={onBulkAction}
               variant="outlined"
-              // TODO: i18n
               options={[
                 {
                   id: 'acceptAll',
@@ -138,7 +137,7 @@ export function RepoStatusUI(props: RepoStatusUIProps) {
       <Box padding={2}>
         <Grid container spacing={2}>
           {status.conflicting.length > 0 && (
-            <Grid item md={12} xl={status.uncommittedChanges.length ? 12 : 12}>
+            <Grid item md={12}>
               <Typography variant="h6" className={cx(classes.sectionLabel, classes.conflictedFilesLabel)}>
                 <FormattedMessage id="repository.conflictedFiles" defaultMessage="Conflicted Files" />
               </Typography>
@@ -180,7 +179,7 @@ export function RepoStatusUI(props: RepoStatusUIProps) {
             </Grid>
           )}
           {status.uncommittedChanges.length > 0 && (
-            <Grid item md={12} xl={status.conflicting.length ? 12 : 12}>
+            <Grid item md={12}>
               <Typography variant="h6" className={classes.sectionLabel}>
                 <FormattedMessage id="repository.pendingCommit" defaultMessage="Pending Commit" />
               </Typography>
