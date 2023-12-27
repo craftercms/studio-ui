c8942d6ddecf8bcbbc0e117b6f60c3952dff2fd2
Roy Art <royart@me.com>
2023-12-27 16:03:19 +0100

    [681e] Update Project lifecycle event messages (#3558)

--- ui/app/src/models/ProjectLifecycleEvent.ts
+++ ui/app/src/models/ProjectLifecycleEvent.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2023 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -14,13 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import Person from './Person';
-
-export interface SocketEventBase {
+export interface ProjectLifecycleEvent<T = 'SITE_READY_EVENT' | 'SITE_DELETING_EVENT' | 'SITE_DELETED_EVENT'> {
   timestamp: number;
-  eventType: string;
-  targetPath: string;
-  user: Person;
+  siteUuid: string;
+  eventType: T;
 }
-
-export default SocketEventBase;
