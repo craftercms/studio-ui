09e877880251d59cd2b88d2a78dc1210a82945d5
Roy Art <royart@me.com>
2024-02-22 16:18:51 -0600

    [800e] Incorrect publishing status when a site is created in the background (#3719)
    * [800e] Incorrect publishing status when a site is created in the background
    craftersoftware/craftercms#800
    
    * Add protection for big site creation publisher status check failed right after creation, add handling for failed publisher status check

--- ui/app/src/components/SiteManagement/SiteManagement.tsx
+++ ui/app/src/components/SiteManagement/SiteManagement.tsx
@@ -26,9 +26,6 @@ import IconButton from '@mui/material/IconButton';
 import { useDispatch } from 'react-redux';
 import LookupTable from '../../models/LookupTable';
 import { PublishingStatus } from '../../models/Publishing';
-import { merge } from 'rxjs';
-import { fetchStatus } from '../../services/publishing';
-import { map } from 'rxjs/operators';
 import { Site } from '../../models/Site';
 import { setSiteCookie } from '../../utils/auth';
 import { trash } from '../../services/sites';
@@ -49,7 +46,6 @@ import { foo, nnou } from '../../utils/object';
 import { useEnv } from '../../hooks/useEnv';
 import { useActiveUser } from '../../hooks/useActiveUser';
 import { useLogicResource } from '../../hooks/useLogicResource';
-import { useMount } from '../../hooks/useMount';
 import { useSpreadState } from '../../hooks/useSpreadState';
 import { useSitesBranch } from '../../hooks/useSitesBranch';
 import Paper from '@mui/material/Paper';
@@ -78,7 +74,6 @@ export function SiteManagement() {
     getStoredGlobalMenuSiteViewPreference(user.username) ?? 'grid'
   );
   const { byId: sitesById, isFetching, active } = useSitesBranch();
-  const [publishingStatusLookup, setPublishingStatusLookup] = useSpreadState<LookupTable<PublishingStatus>>({});
   const [selectedSiteStatus, setSelectedSiteStatus] = useState<PublishingStatus>(null);
   const [permissionsLookup, setPermissionsLookup] = useState<LookupTable<boolean>>(foo);
   const [sitesRefreshCountLookup, setSitesRefreshCountLookup] = useSpreadState<LookupTable<number>>({});
@@ -87,23 +82,11 @@ export function SiteManagement() {
   const [isDuplicateDialogFromCreateDialog, setIsDuplicateDialogFromCreateDialog] = useState(false);
 
   useEffect(() => {
-    merge(
-      ...Object.keys(sitesById).map((siteId) =>
-        fetchStatus(siteId).pipe(
-          map((status) => ({
-            status,
-            siteId
-          }))
-        )
-      )
-    ).subscribe(({ siteId, status }) => {
-      setPublishingStatusLookup({ [siteId]: status });
-    });
-  }, [setPublishingStatusLookup, sitesById]);
-
-  useMount(() => {
-    hasGlobalPermissions('create_site', 'edit_site', 'delete_site', 'duplicate_site').subscribe(setPermissionsLookup);
-  });
+    const subscription = hasGlobalPermissions('create_site', 'edit_site', 'delete_site', 'duplicate_site').subscribe(
+      setPermissionsLookup
+    );
+    return () => subscription.unsubscribe();
+  }, []);
 
   const resource = useLogicResource<Site[], { sitesById: LookupTable<Site>; isFetching: boolean }>(
     useMemo(
@@ -184,10 +167,14 @@ export function SiteManagement() {
     );
   };
 
-  const onPublishButtonClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, site: Site) => {
+  const onPublishButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    site: Site,
+    status: PublishingStatus
+  ) => {
     event.preventDefault();
     event.stopPropagation();
-    setSelectedSiteStatus(publishingStatusLookup[site.id]);
+    setSelectedSiteStatus(status);
     publishingStatusDialogState.onOpen();
   };
 
@@ -296,7 +283,6 @@ export function SiteManagement() {
         >
           <SitesGrid
             resource={resource}
-            publishingStatusLookup={publishingStatusLookup}
             onSiteClick={onSiteClick}
             onDeleteSiteClick={permissionsLookup['delete_site'] && onDeleteSiteClick}
             onEditSiteClick={permissionsLookup['edit_site'] && onEditSiteClick}
