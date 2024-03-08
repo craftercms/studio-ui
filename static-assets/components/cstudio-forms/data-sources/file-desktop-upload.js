b8a15c4b7a76a436e64ad97160d1c0637cee6513
Jose Javier Vega <josevega19@gmail.com>
2024-03-08 09:00:30 -0600

    [6533] Node selector control should only provide the user with an Edit option for content items that are editable by Studio (#3706)
    * Only add edit button if content is embedded or under /site, or if is editable asset #6533
    
    * Add case for uploaded files when rendering label in node selector as they were being labeled as embedded #6533
    
    * Properly align action buttons in node selector #6533
    
    * Edit content in file-desktop-upload instead of showing upload dialog on edit #6533
    
    * Validate string value of item.inline when displaying node-selector item's label #6533

--- static-assets/components/cstudio-forms/data-sources/file-desktop-upload.js
+++ static-assets/components/cstudio-forms/data-sources/file-desktop-upload.js
@@ -101,48 +101,18 @@ YAHOO.extend(CStudioForms.Datasources.FileDesktopUpload, CStudioForms.CStudioFor
   },
 
   edit: function (key, control) {
-    this._self = this;
-    var me = this;
-
-    var site = CStudioAuthoringContext.site;
-    var path = this._self.repoPath;
-    var isUploadOverwrite = true;
-
-    for (var i = 0; i < this.properties.length; i++) {
-      if (this.properties[i].name === 'repoPath') {
-        path = this.properties[i].value;
-
-        path = this.processPathsForMacros(path);
+    CStudioAuthoring.Service.lookupContentItem(CStudioAuthoringContext.site, key, {
+      success: function (contentTO) {
+        CStudioAuthoring.Operations.editContent(
+          contentTO.item.contentType,
+          CStudioAuthoringContext.siteId,
+          contentTO.item.mimeType,
+          contentTO.item.nodeRef,
+          contentTO.item.uri,
+          false
+        );
       }
-    }
-
-    var callback = {
-      success: function (fileData) {
-        if (control) {
-          control.deleteItem(key);
-          control.insertItem(
-            path + '/' + fileData.fileName,
-            path + '/' + fileData.fileName,
-            fileData.fileExtension,
-            fileData.size,
-            me.id
-          );
-          if (control._renderItems) {
-            control._renderItems();
-          }
-        }
-      },
-
-      failure: function () {
-        if (control) {
-          control.failure('An error occurred while uploading the file.');
-        }
-      },
-
-      context: this
-    };
-
-    CStudioAuthoring.Operations.uploadAsset(site, path, isUploadOverwrite, callback);
+    });
   },
 
   getLabel: function () {
