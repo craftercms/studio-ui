baa3c3e32faffcae6b22dd53f9ee64520201e4d6
jvega190 <josevega19@gmail.com>
2024-05-20 10:12:58 -0600

    [6714] History "Quick Compare" options should always use the same diffing direction

--- ui/app/src/components/CompareVersionsDialog/CompareVersionsDialogContainer.tsx
+++ ui/app/src/components/CompareVersionsDialog/CompareVersionsDialogContainer.tsx
@@ -98,7 +98,15 @@ export function CompareVersionsDialogContainer(props: CompareVersionsDialogConta
     if (!selected[0]) {
       dispatch(compareVersion({ id: version.versionNumber }));
     } else if (selected[0] !== version.versionNumber) {
-      dispatch(compareBothVersions({ versions: [selected[0], version.versionNumber] }));
+      const selectedADate = new Date(versionsBranch.byId[selected[0]].modifiedDate);
+      const selectedBDate = new Date(version.modifiedDate);
+
+      dispatch(
+        compareBothVersions({
+          versions:
+            selectedADate > selectedBDate ? [selected[0], version.versionNumber] : [version.versionNumber, selected[0]]
+        })
+      );
     } else {
       dispatch(compareVersion());
     }
