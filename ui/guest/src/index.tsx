1aacab408f259529d50197781406bf7430d3d580
jvega190 <josevega19@gmail.com>
2024-07-04 14:54:18 -0600

    [6983] Add registerElements and deregisterElements to library

--- ui/guest/src/index.tsx
+++ ui/guest/src/index.tsx
@@ -111,3 +111,28 @@ export const guestCheckIn$ = guestCheckInInternal$.pipe(
 export { elementRegistry, iceRegistry, contentController, fromTopic, post };
 
 typeof document !== 'undefined' && setTimeout(() => document?.dispatchEvent(new CustomEvent(xbLoadedEvent)));
+
+export function registerElements(container: HTMLElement) {
+  if (container) {
+    container.querySelectorAll('[data-craftercms-model-id]').forEach((element) => {
+      elementRegistry.register({
+        element: element as HTMLElement,
+        path: element.getAttribute('data-craftercms-model-path'),
+        modelId: element.getAttribute('data-craftercms-model-id'),
+        fieldId: element.getAttribute('data-craftercms-field-id'),
+        index: element.getAttribute('data-craftercms-index'),
+        label: element.getAttribute('data-craftercms-label')
+      });
+    });
+  }
+}
+
+export function deregisterElements(container: HTMLElement) {
+  if (container) {
+    const articleElements = container.querySelectorAll('[data-craftercms-model-id]');
+    articleElements.forEach((element) => {
+      let record = elementRegistry.fromElement(element);
+      elementRegistry.deregister(record?.id);
+    });
+  }
+}
