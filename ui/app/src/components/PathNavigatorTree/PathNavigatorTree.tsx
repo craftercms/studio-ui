0833bfac53f0ac5b9b38176a2d9a17c784ee3e83
Jose Javier Vega <josevega19@gmail.com>
2024-07-11 14:05:33 -0600

    [6988] Path selection not working with 31 folders (#4013)
    * [6988] Consider custom limits in limits list of PathNavigatorTree
    
    * [6988] Add showPaginationOptions prop to PathNavigatorTree, include limit prop as part of limits showing in pagination options
    
    * [6988] Include level descriptor into children total

--- ui/app/src/components/PathNavigatorTree/PathNavigatorTree.tsx
+++ ui/app/src/components/PathNavigatorTree/PathNavigatorTree.tsx
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import PathNavigatorTreeUI, { PathNavigatorTreeUIProps } from './PathNavigatorTreeUI';
 import { useDispatch } from 'react-redux';
 import {
@@ -90,6 +90,7 @@ export interface PathNavigatorTreeProps
   onNodeClick?: PathNavigatorTreeUIProps['onLabelClick'];
   active?: PathNavigatorTreeItemProps['active'];
   classes?: Partial<Record<'header', string>>;
+  showPaginationOptions?: boolean;
 }
 
 export interface PathNavigatorTreeStateProps {
@@ -134,6 +135,7 @@ interface Menu {
 //   }
 // };
 
+export const limitsDefault = [5, 10, 25, 50];
 export function PathNavigatorTree(props: PathNavigatorTreeProps) {
   // region const { ... } = props;
   const {
@@ -158,7 +160,8 @@ export function PathNavigatorTree(props: PathNavigatorTreeProps) {
     showWorkflowState,
     showItemMenu,
     sortStrategy,
-    order
+    order,
+    showPaginationOptions = true
   } = props;
   // endregion
   const state = useSelection((state) => state.pathNavigatorTree[id]);
@@ -177,6 +180,9 @@ export function PathNavigatorTree(props: PathNavigatorTreeProps) {
   const errorByPath = state?.errorByPath;
   const getItemByPath = (path: string) => lookupItemByPath(path, itemsByPath);
   const rootItem = getItemByPath(rootPath);
+  const limits = useMemo(() => {
+    return [...new Set([...limitsDefault, limit])].sort((a, b) => a - b);
+  }, [limit]);
 
   useEffect(() => {
     // Adding uiConfig as means to stop navigator from trying to
@@ -373,8 +379,10 @@ export function PathNavigatorTree(props: PathNavigatorTreeProps) {
         showPublishingTarget={showPublishingTarget}
         showWorkflowState={showWorkflowState}
         showItemMenu={showItemMenu}
+        limits={limits}
         limit={state.limit}
         onLimitChange={onLimitChange}
+        showPaginationOptions={showPaginationOptions}
       />
       <ContextMenu
         anchorEl={widgetMenu.anchorEl}
