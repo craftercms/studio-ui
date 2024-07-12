16b6f2d64427a95ec9149390402055d71e0dfe14
Jose Javier Vega <josevega19@gmail.com>
2024-07-12 07:38:50 -0600

    [6983] Improve XB ease-of-use and resilience for JS apps programmatically registering elements for XB (#4018)
    * [6983] Improve XB efficiency, resilliance and ease-of-use for element registration
    
    * [6983] Improve XB ease-of-use and resilience for JS apps programmatically registering elements for XB
    
    * [6988] Remove comment
    
    * [6983] Clean recordIdByElementLookup on deregister, cancel registration subscriptions when deregistering
    
    * [6983] Add registerElements and deregisterElements to library
    
    * [6983] Update to have a lookup of subscriptions to cancel when deregistering
    
    * [6983] Update variable articleElements => elements
    
    * [6983] Add deregister$ stream of ids being deregistered
    
    * [6983] Update deregister$ Subject to be of type string
    
    ---------
    
    Co-authored-by: rart <royart@me.com>

--- ui/guest/src/react/ExperienceBuilder.tsx
+++ ui/guest/src/react/ExperienceBuilder.tsx
@@ -77,7 +77,7 @@ import {
 } from '@craftercms/studio-ui/state/actions/preview';
 import { createGuestStore } from '../store/store';
 import { Provider } from 'react-redux';
-import { clearAndListen$ } from '../store/subjects';
+import { clearAndListen$, guestCheckIn$ } from '../store/subjects';
 import { GuestState } from '../store/models/GuestStore';
 import { nnou, nullOrUndefined } from '@craftercms/studio-ui/utils/object';
 import { scrollToDropTargets } from '../utils/dom';
@@ -521,6 +521,7 @@ function ExperienceBuilderInternal(props: InternalGuestProps) {
       });
 
     post(guestCheckIn({ location, path, site, version: process.env.VERSION }));
+    guestCheckIn$.next(true);
 
     return () => {
       post(guestCheckOut({ path }));
@@ -529,6 +530,7 @@ function ExperienceBuilderInternal(props: InternalGuestProps) {
       refs.current.contentReady = false;
       flushRequestedPaths();
       operationsSubscription.unsubscribe();
+      guestCheckIn$.next(false);
     };
   }, [dispatch, path]);
 
