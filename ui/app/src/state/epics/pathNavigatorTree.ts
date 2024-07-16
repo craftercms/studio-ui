b3ac484cd0cb661c3a68356e0a52833bb9082479
jvega190 <josevega19@gmail.com>
2024-07-16 16:15:35 -0600

    [6033] Remove unused imports

--- ui/app/src/state/epics/pathNavigatorTree.ts
+++ ui/app/src/state/epics/pathNavigatorTree.ts
@@ -25,7 +25,6 @@ import {
   pathNavigatorTreeBulkRefresh,
   pathNavigatorTreeBulkRestoreComplete,
   pathNavigatorTreeBulkRestoreFailed,
-  pathNavigatorTreeChangeLimit,
   pathNavigatorTreeCollapsePath,
   pathNavigatorTreeExpandPath,
   pathNavigatorTreeFetchPathChildren,
