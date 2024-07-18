271b658b7071d777b9ac689f381f38e02ccc7cc7
Jose Javier Vega <josevega19@gmail.com>
2024-07-18 11:38:38 -0600

    [7017] Search feature issues on PathNavTree (#4045)
    * [7017] Validate item exists
    
    * [7017] Search feature issues on PathNavTree
    
    * [7017] Set totalByPath of non expanded items
    
    * [7017] Remove filter to avoid additional iteration
    
    * [7017] Update initialize childrenByParentPath with empty array comment
    
    * [7017] Remove dispatching of pathNavigatorTreeBackgroundRefresh as it is not needed, only validate if children total exists in restoreTree

--- ui/app/src/components/PathNavigatorTree/PathNavigatorTree.tsx
+++ ui/app/src/components/PathNavigatorTree/PathNavigatorTree.tsx
@@ -201,14 +201,11 @@ export function PathNavigatorTree(props: PathNavigatorTreeProps) {
   useEffect(() => {
     const subscription = onSearch$.pipe(debounceTime(400)).subscribe(({ keyword, path }) => {
       dispatch(
-        batchActions([
-          pathNavigatorTreeSetKeyword({
-            id,
-            path,
-            keyword
-          }),
-          pathNavigatorTreeBackgroundRefresh({ id })
-        ])
+        pathNavigatorTreeSetKeyword({
+          id,
+          path,
+          keyword
+        })
       );
     });
     return () => {
