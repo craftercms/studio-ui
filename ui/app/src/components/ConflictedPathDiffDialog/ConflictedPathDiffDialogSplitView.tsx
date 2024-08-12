695cc10594168343d2d9ce81e022327019378c63
jvega190 <josevega19@gmail.com>
2024-08-12 10:40:20 -0600

    Revert "[7136] Reduce split view breapoint"
    This reverts commit 5c6075a781e0868255cc4e20ea59f88fc7a20b58.

--- ui/app/src/components/ConflictedPathDiffDialog/ConflictedPathDiffDialogSplitView.tsx
+++ ui/app/src/components/ConflictedPathDiffDialog/ConflictedPathDiffDialogSplitView.tsx
@@ -36,9 +36,7 @@ export function ConflictedPathDiffDialogSplitView(props: SplitViewProps) {
           scrollbar: {
             alwaysConsumeMouseWheel: false
           },
-          readOnly: true,
-          // Monaco editor has a breakpoint for split view, we had to decrease it for the split view to show in current dialog
-          renderSideBySideInlineBreakpoint: 300
+          readOnly: true
         });
         diffEditor.setModel({
           original: studioVersion,
