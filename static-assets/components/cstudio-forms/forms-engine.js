40729c3dcc40c3cceea30e5e4fcb985c41988b3e
jvega190 <josevega19@gmail.com>
2024-09-05 10:33:41 -0600

    [7238] Remove saveDraft and saveMinimize options for embedded forms, update SplitButtonUI to show only the button when only one option

--- static-assets/components/cstudio-forms/forms-engine.js
+++ static-assets/components/cstudio-forms/forms-engine.js
@@ -1349,7 +1349,7 @@ const initializeCStudioForms = () => {
               var folderName =
                 form.definition.contentAsFolder || form.definition.contentAsFolder === 'true'
                   ? useCurrentValidFolder
-                    ? CStudioForms.currentValidFolder ?? CStudioForms.initialModel['folder-name']
+                    ? (CStudioForms.currentValidFolder ?? CStudioForms.initialModel['folder-name'])
                     : form.model['folder-name']
                   : undefined;
               /*
@@ -1909,7 +1909,8 @@ const initializeCStudioForms = () => {
               // This is really the right thing to do but previewable doesn't come through
               CStudioAuthoring.Service.lookupContentType(CStudioAuthoringContext.site, contentType, {
                 success: function (type) {
-                  const storedId = 'formEditor';
+                  const isEmbeddedForm = window.location.href.includes('isInclude=true');
+                  const storedId = isEmbeddedForm ? 'formEditorEmbedded' : 'formEditor';
                   const defaultSelected = 'saveAndClose';
 
                   const onMultiChoiceSaveButtonClick = (e, type) => {
@@ -1965,21 +1966,29 @@ const initializeCStudioForms = () => {
                     disablePortal: false,
                     storageKey: storedId,
                     options: [
-                      {
-                        id: 'saveDraft',
-                        label: formatMessage(formEngineMessages.saveDraft),
-                        callback: (e) => onMultiChoiceSaveButtonClick(e, 'save')
-                      },
+                      ...(!isEmbeddedForm
+                        ? [
+                            {
+                              id: 'saveDraft',
+                              label: formatMessage(formEngineMessages.saveDraft),
+                              callback: (e) => onMultiChoiceSaveButtonClick(e, 'save')
+                            }
+                          ]
+                        : []),
                       {
                         id: 'saveAndClose',
                         label: formatMessage(formEngineMessages.saveAndClose),
                         callback: (e) => onMultiChoiceSaveButtonClick(e, 'saveAndClose')
                       },
-                      {
-                        id: 'saveAndMinimize',
-                        label: formatMessage(formEngineMessages.saveAndMinimize),
-                        callback: (e) => onMultiChoiceSaveButtonClick(e, 'saveAndMinimize')
-                      }
+                      ...(!isEmbeddedForm
+                        ? [
+                            {
+                              id: 'saveAndMinimize',
+                              label: formatMessage(formEngineMessages.saveAndMinimize),
+                              callback: (e) => onMultiChoiceSaveButtonClick(e, 'saveAndMinimize')
+                            }
+                          ]
+                        : [])
                     ]
                   });
                 },
