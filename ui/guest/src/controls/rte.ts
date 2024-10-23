436977c38621bf0151d141876cc15835d3319835
jvega190 <josevega19@gmail.com>
2024-10-23 16:41:13 -0600

    [7433] Able to bypass character limit in Simple Text field through XB

--- ui/guest/src/controls/rte.ts
+++ ui/guest/src/controls/rte.ts
@@ -112,6 +112,7 @@ export function initTinyMCE(
 
   record.element.classList.remove(emptyFieldClass);
 
+  const maxLength = validations?.maxLength ? parseInt(validations.maxLength.value) : null;
   window.tinymce.init({
     license_key: 'gpl',
     target: rteEl,
@@ -125,8 +126,24 @@ export function initTinyMCE(
     plugins: ['craftercms_paste editform', rteSetup?.tinymceOptions?.plugins].filter(Boolean).join(' '), // 'editform' plugin will always be loaded
     paste_as_text: type !== 'html',
     paste_data_images: type === 'html',
-    paste_preprocess(plugin, args) {
-      window.tinymce.activeEditor.plugins.craftercms_paste_extension?.paste_preprocess(plugin, args);
+    paste_preprocess(editor, args) {
+      const currentContent = editor.getContent({ format: 'text' });
+      const fullContent = currentContent + args.content;
+      const maxLengthExceeded = maxLength && fullContent.length > maxLength;
+      if (maxLengthExceeded) {
+        post(
+          snackGuestMessage({
+            id: 'maxLength',
+            level: 'required',
+            values: {
+              maxLength: args.content.length === maxLength ? fullContent.length : `${fullContent.length}/${maxLength}`
+            }
+          })
+        );
+        args.content = args.content.substring(0, maxLength - currentContent.length);
+      }
+
+      window.tinymce.activeEditor.plugins.craftercms_paste_extension?.paste_preprocess(editor, args);
     },
     paste_postprocess(plugin, args) {
       window.tinymce.activeEditor.plugins.craftercms_paste_extension?.paste_postprocess(plugin, args);
@@ -329,21 +346,6 @@ export function initTinyMCE(
       });
 
       editor.on('paste', (e) => {
-        const maxLength = validations?.maxLength ? parseInt(validations.maxLength.value) : null;
-        const text = (
-          e.clipboardData ||
-          // @ts-ignore
-          window.clipboardData
-        ).getData('text');
-        if (maxLength && text.length > maxLength) {
-          post(
-            snackGuestMessage({
-              id: 'maxLength',
-              level: 'required',
-              values: { maxLength: text.length === maxLength ? text.length : `${text.length}/${maxLength}` }
-            })
-          );
-        }
         if (type === 'textarea') {
           // Doing this immediately (without the timeout) causes the content to be duplicated.
           // TinyMCE seems to be doing something internally that causes this.
@@ -356,7 +358,6 @@ export function initTinyMCE(
             }
           }, 10);
         }
-        // TODO: It'd be great to be able to select the piece of the pasted content that falls out of the max-length.
       });
 
       editor.on('keyup', (e) => {
