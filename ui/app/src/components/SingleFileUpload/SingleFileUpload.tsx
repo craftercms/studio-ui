f5a8f0e604eaa708c128bc68dade061732bafa0b
Jose Javier Vega <josevega19@gmail.com>
2025-03-17 13:01:11 -0600

    [7826] Video upload to S3 end up multiples upload (#4428)
    * [7826] Video upload to S3 end up multiples upload
    
    * [7826] Remove xsrf in url since the uploader already sets it in the headers
    
    * Update ui/app/src/components/SingleFileUpload/SingleFileUpload.tsx
    
    * Update ui/app/src/components/SingleFileUpload/SingleFileUpload.tsx
    
    * Update ui/app/src/components/SingleFileUpload/SingleFileUpload.tsx
    
    ---------
    
    Co-authored-by: Roy Art <3928341+rart@users.noreply.github.com>

--- ui/app/src/components/SingleFileUpload/SingleFileUpload.tsx
+++ ui/app/src/components/SingleFileUpload/SingleFileUpload.tsx
@@ -285,7 +285,9 @@ export function SingleFileUpload(props: SingleFileUploadProps) {
             setConfirm({ body: message });
             setSuggestedName(modifiedName);
           } else {
-            uppy.upload();
+            // When uploading large files to aws/s3, something causes requests to fail and get retried n times before finally stating it failed; despite the file seemingly actually getting uploaded.
+            // This setTimeout avoids that issue. The mechanism of failure or why this avoids it is unknown.
+            setTimeout(() => uppy.upload());
             setDescription(`${formatMessage(messages.uploadingFile)}:`);
             onUploadStart?.();
           }
@@ -306,7 +308,7 @@ export function SingleFileUpload(props: SingleFileUploadProps) {
   }, [onUploadStart, formatMessage, path, site, uppy]);
 
   const onConfirm = () => {
-    uppy.upload().then(() => {});
+    uppy.upload();
     setSuggestedName(null);
     setDescription(`${formatMessage(messages.uploadingFile)}:`);
     onUploadStart?.();
