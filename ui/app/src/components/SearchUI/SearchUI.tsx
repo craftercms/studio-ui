1b4b98aa5ea93c5f281ba00ee8b75bdbae617f2a
Jose Javier Vega <josevega19@gmail.com>
2025-03-18 10:30:18 -0600

    [4.x][7558] Remove label from Search's select all checkbox, replace with tooltip (#4432)
    * [7558] Remove label from Search's select all checkbox, replace with tooltip
    
    * [7558] Remove unused imports
    
    * [7558] Adjust checkbox positioning

--- ui/app/src/components/SearchUI/SearchUI.tsx
+++ ui/app/src/components/SearchUI/SearchUI.tsx
@@ -23,10 +23,7 @@ import palette from '../../styles/palette';
 import { ElasticParams, Filter, MediaItem, SearchResult } from '../../models/Search';
 import { CheckedFilter, drawerWidth } from '../Search/utils';
 import LookupTable from '../../models/LookupTable';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Typography from '@mui/material/Typography';
 import { translations } from '../Search/translations';
 import TablePagination from '@mui/material/TablePagination';
 import ApiResponseErrorState from '../ApiResponseErrorState';
@@ -44,6 +41,7 @@ import IconButton from '@mui/material/IconButton';
 import MoreVertRounded from '@mui/icons-material/MoreVertRounded';
 import { UNDEFINED } from '../../utils/constants';
 import { LoadingState } from '../LoadingState';
+import Tooltip from '@mui/material/Tooltip';
 
 export interface SearchUIProps {
   selectedPath: string;
@@ -125,7 +123,7 @@ const useStyles = makeStyles()((theme) => ({
   },
   searchHelperBar: {
     display: 'flex',
-    padding: '0 6px 0 20px',
+    padding: '0 6px',
     alignItems: 'center',
     background: theme.palette.background.paper,
     borderBottom: `1px solid ${theme.palette.divider}`
@@ -372,18 +370,9 @@ export function SearchUI(props: SearchUIProps) {
         }
       >
         <div className={classes.searchHelperBar}>
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  color="primary"
-                  checked={areAllSelected}
-                  onClick={(e: any) => handleSelectAll(e.target.checked)}
-                />
-              }
-              label={<Typography color="textPrimary">{formatMessage(translations.selectAll)}</Typography>}
-            />
-          </FormGroup>
+          <Tooltip title={<FormattedMessage defaultMessage="Select All on this page" />}>
+            <Checkbox checked={areAllSelected} onChange={(e) => handleSelectAll(e.target.checked)} />
+          </Tooltip>
           <TablePagination
             rowsPerPageOptions={[9, 15, 21]}
             className={classes.pagination}
