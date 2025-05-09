cbf0e3d511bad60086996065886c9fabd095dbe4
jvega190 <josevega19@gmail.com>
2025-05-09 15:35:37 -0600

    [7890] Fix XB dissociate action race condition

--- ui/guest/src/react/ExperienceBuilder.tsx
+++ ui/guest/src/react/ExperienceBuilder.tsx
@@ -636,6 +636,8 @@ function ExperienceBuilderInternal(props: InternalGuestProps) {
 							const hasValidations = Boolean(validations.length);
 							const hasFailedRequired = validations.some(({ level }) => level === 'required');
 							const elementRecord = elementRegistry.get(highlight.id);
+							// If no elementRecord is found, the item was removed while the hover was on top. Skip.
+							if (!elementRecord) return null;
 							const elementPath = models[elementRecord.modelId]?.craftercms.path ?? path;
 							const { isLocked, isExternallyModified, isLockedByCurrentUser } = checkIfLockedOrModified(
 								state,
