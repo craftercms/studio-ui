0b143efd61aa76b7c2b1c2d3cefbdc8fa71fbc1e
jvega190 <josevega19@gmail.com>
2025-05-30 11:26:25 -0600

    [8154] Site breaks when we try to duplicate an item

--- ui/guest/src/store/util.ts
+++ ui/guest/src/store/util.ts
@@ -123,9 +123,9 @@ export const checkIfLockedOrModified = (state: GuestState, record: ElementRecord
 			modelId = model.craftercms.id;
 		}
 	}
-	const parentModelId = model.craftercms.path ? null : getParentModelId(modelId, getCachedModels(), modelHierarchyMap);
+	const parentModelId = model?.craftercms.path ? null : getParentModelId(modelId, getCachedModels(), modelHierarchyMap);
 	const parentModel = parentModelId ? getCachedModel(parentModelId) : null;
-	const path = model.craftercms.path ?? parentModel.craftercms.path;
+	const path = model?.craftercms.path ?? parentModel.craftercms.path;
 	const isLocked = Boolean(state.lockedPaths[path]);
 	const isLockedByCurrentUser = isLocked && state.lockedPaths[path].user.username === state.username;
 	const isExternallyModified = Boolean(state.externallyModifiedPaths[path]);
