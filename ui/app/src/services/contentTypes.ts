1ef981f818999fbae882e3a0857eb4b694d6a845
jvega190 <josevega19@gmail.com>
2025-06-05 12:10:04 -0600

    [7418] Rollback TODO comment

--- ui/app/src/services/contentTypes.ts
+++ ui/app/src/services/contentTypes.ts
@@ -356,7 +356,6 @@ function parseLegacyFormDefinitionFields(
 				case 'minSize':
 					break;
 				default:
-					// TODO: check 'unhandled constraint "maxLength" and other possible constraints
 					console.log(`[parseLegacyFormDef] Unhandled constraint "${legacyProp.name}"`, legacyProp);
 			}
 		});
