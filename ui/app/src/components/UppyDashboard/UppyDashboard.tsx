684047f8fa9e95253af1df3430e96cc2bc0def42
jvega190 <josevega19@gmail.com>
2025-06-19 14:21:11 -0600

    Restore ts-expect-error comment, add proper DashboardOptions type to options object

--- ui/app/src/components/UppyDashboard/UppyDashboard.tsx
+++ ui/app/src/components/UppyDashboard/UppyDashboard.tsx
@@ -30,6 +30,7 @@ import { ensureSingleSlash } from '../../utils/string';
 import { UppyDashboardProps } from './UppyDashboardProps';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Box from '@mui/material/Box';
+import type { DashboardOptions } from '@uppy/dashboard';
 
 const translations = defineMessages({
 	cancelPending: {
@@ -151,6 +152,7 @@ export function UppyDashboard(props: UppyDashboardProps) {
 			inline: true,
 			target: ref.current,
 			// TODO: Check our Dashboard component typings
+			// @ts-expect-error validateActionPolicy is a prop of @craftercms/uppy/plugins/Dashboard, and not in the type definition of uppy's dashboard.
 			validateActionPolicy,
 			onPendingChanges: function () {
 				functionsRef.current.onPendingChanges.apply(null, arguments);
@@ -202,7 +204,7 @@ export function UppyDashboard(props: UppyDashboardProps) {
 					})
 				);
 			}
-		});
+		} as DashboardOptions<Meta, Body>);
 
 		const onUploadSuccess = (file: UppyFile<Meta, Body>) => {
 			onItemsUploaded$.next(file.id);
