2df5dcaccbf31e4dcf990dd95788048ad735d9b8
jvega190 <josevega19@gmail.com>
2025-07-01 08:55:56 -0600

    [7954] Revert change the return signature of _renderInputMarkup, use element from this property

--- static-assets/components/cstudio-forms/controls/rte.js
+++ static-assets/components/cstudio-forms/controls/rte.js
@@ -283,7 +283,8 @@ CStudioAuthoring.Module.requireModule(
 					this.rteId = rteId;
 					this.rteWidth = '100%';
 
-					const { inputEl, onDemandEditorInitializationEl } = this._renderInputMarkup(config, rteId);
+					const inputEl = this._renderInputMarkup(config, rteId);
+					const onDemandEditorInitializationEl = this.onDemandEditorInitializationEl;
 
 					const initTiny = () => {
 						// Getting properties from content-type
@@ -530,7 +531,7 @@ CStudioAuthoring.Module.requireModule(
 					if (config.onDemandEditorInitialization) {
 						onDemandEditorInitializationEl.addEventListener('click', () => {
 							initTiny();
-							onDemandEditorInitializationEl.style.display = 'none';
+							onDemandEditorInitializationEl.remove();
 						});
 					} else {
 						initTiny();
@@ -885,7 +886,7 @@ CStudioAuthoring.Module.requireModule(
 					this.containerEl.appendChild(controlWidgetContainerEl);
 					controlWidgetContainerEl.appendChild(descriptionEl);
 
-					return { inputEl, onDemandEditorInitializationEl };
+					return inputEl;
 				},
 
 				/**
