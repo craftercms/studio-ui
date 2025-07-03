e8cfc65bf163d402cea67a70d2350408d7fd2527
jvega190 <josevega19@gmail.com>
2025-07-03 08:41:07 -0600

    [7954] Page become unresponsive with form with multiple RTEs

--- static-assets/components/cstudio-forms/controls/rte.js
+++ static-assets/components/cstudio-forms/controls/rte.js
@@ -510,10 +510,13 @@ CStudioAuthoring.Module.requireModule(
 						})
 					});
 
+					const isInRepeatGroup = Boolean(config.repeatContainer);
 					// Update all content before saving the form (all content is automatically updated on focusOut)
-					callback = {};
-					callback.beforeSave = function () {
-						_thisControl.save();
+					callback = {
+						beforeSave: function () {
+							_thisControl.save();
+						},
+						...(isInRepeatGroup ? { repeatGroupId: config.repeatContainer.id } : {})
 					};
 					_thisControl.form.registerBeforeSaveCallback(callback);
 
