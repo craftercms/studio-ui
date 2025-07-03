59ff61aedb5f15313bc0bc9101120126861a1d57
Jose Javier Vega <josevega19@gmail.com>
2025-07-03 12:08:14 -0600

    [4.x][8233] Flashing menu and poor performance in loading RTEs (#4571)
    * [8233] Avoid re-rendering whole repeat group when adding/removing/moving items
    
    * [8233] Sync control ids when updating repeat group items
    
    * [8233] Check for editor before removing instance, validate repeat field id before updating
    
    * [8233] Update queries to ensure that only immediate childs are selected
    
    * [8233] Remove commented code, rename _reRenderItemsActions => _reRenderAllRepeatItemsActions and _renderItemActions => _renderRepeatItemActions

--- static-assets/components/cstudio-forms/controls/rte.js
+++ static-assets/components/cstudio-forms/controls/rte.js
@@ -521,7 +521,7 @@ CStudioAuthoring.Module.requireModule(
             beforeSave: function () {
               _thisControl.save();
             },
-            ...(isInRepeatGroup ? { repeatGroupId: config.repeatContainer.id } : {})
+            ...(isInRepeatGroup ? { repeatGroupId: config.repeatContainer.id, rteId } : {})
           };
           _thisControl.form.registerBeforeSaveCallback(callback);
 
