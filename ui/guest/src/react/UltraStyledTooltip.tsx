d87473282078cbbf60690a8131c78d1db7148d9a
jvega190 <josevega19@gmail.com>
2025-07-18 14:26:47 -0600

    [8171] Remove non-existing prop `slotProps.tooltip.sx` from Tooltip

--- ui/guest/src/react/UltraStyledTooltip.tsx
+++ ui/guest/src/react/UltraStyledTooltip.tsx
@@ -22,7 +22,7 @@ export const UltraStyledTooltip = (props: TooltipProps) => {
       {...props}
       slotProps={{
         ...props?.slotProps,
-        tooltip: { ...props.slotProps?.tooltip, sx: { ...props.slotProps?.tooltip?.sx, fontSize: 12 } }
+        tooltip: { ...props.slotProps?.tooltip, sx: { fontSize: 12 } }
       }}
     />
   );
