d136696973be9d2aff51e363dfe664577c52b9cf
jvega190 <josevega19@gmail.com>
2025-07-22 10:39:55 -0600

    [8171] Update slotProps.tooltip type

--- ui/guest/src/react/UltraStyledTooltip.tsx
+++ ui/guest/src/react/UltraStyledTooltip.tsx
@@ -22,9 +22,11 @@ export const UltraStyledTooltip = (props: TooltipProps) => {
       {...props}
       slotProps={{
         ...props?.slotProps,
-        // TODO: Ticket reported here: https://github.com/mui/material-ui/issues/46582. Need to fix this when MUI fixes the issue.
-        // @ts-expect-error [TS2339]: Property 'sx' does not exist on type TooltipProps['slotProps]['tooltip'].
-        tooltip: { ...props.slotProps?.tooltip, sx: { fontSize: 12, ...props.slotProps?.tooltip?.sx } }
+        tooltip: {
+          ...props.slotProps?.tooltip,
+          // TODO: handle slotProps of type `function` case.
+          sx: { ...(props.slotProps.tooltip as TooltipProps)?.sx, fontSize: 12 }
+        }
       }}
     />
   );
