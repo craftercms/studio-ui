f24274217ab4a02dafc483272514fa6a4c5c57a0
jvega190 <josevega19@gmail.com>
2025-07-23 11:53:23 -0600

    [7707] Update ItemDisplay item prop to be of type `LightItem | ContentItem`

--- ui/app/src/components/ItemDisplay/ItemDisplay.tsx
+++ ui/app/src/components/ItemDisplay/ItemDisplay.tsx
@@ -40,7 +40,7 @@ export interface ItemDisplayProps<LabelTypographyComponent extends React.Element
 	showNavigableAsLinks?: boolean;
 	classes?: Partial<Record<ItemDisplayClassKey, string>>;
 	sxs?: PartialSxRecord<ItemDisplayClassKey>;
-	item: LightItem & Partial<ContentItem>;
+	item: LightItem | ContentItem;
 	labelTypographyProps?: TypographyProps<LabelTypographyComponent, { component?: LabelTypographyComponent }>;
 	isNavigableFn?: (item: ContentItem) => boolean;
 	labelComponent?: ElementType;
@@ -81,10 +81,10 @@ const ItemDisplay = forwardRef<HTMLSpanElement, ItemDisplayProps>((props, ref) =
 		// Prevents crashing if the item is nullish
 		return null;
 	}
-	const isDisabledItem = item.stateMap?.disabled;
+	const isDisabledItem = (item as ContentItem).stateMap?.disabled;
 	// inWorkflow will only be true for type ContentItem (if they met the workflow criteria). Casting to ContentItem
 	// is only done on scenarios where `isWorkflow` is true.
-	const inWorkflow = isInWorkflow(item.stateMap) || item.systemType === 'folder';
+	const inWorkflow = isInWorkflow((item as ContentItem).stateMap) || item.systemType === 'folder';
 	return (
 		<Box
 			component={component}
