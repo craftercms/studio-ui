90022369d7487d27d9d1e0a8034bb9ebdbd2a105
jvega190 <josevega19@gmail.com>
2025-08-11 16:57:04 -0600

    [8319] Publish Approval dialog should default to the NOW option when approving a request that was subitted with NOW as the preferred publish time

--- ui/app/src/utils/itemActions.ts
+++ ui/app/src/utils/itemActions.ts
@@ -965,7 +965,7 @@ export const itemActionDispatcher = ({
     case 'schedulePublish':
     case 'requestPublish': {
       const schedulingMap = {
-        approvePublish: null,
+        approvePublish: undefined,
         schedulePublish: 'custom',
         requestPublish: 'now',
         publish: 'now'
