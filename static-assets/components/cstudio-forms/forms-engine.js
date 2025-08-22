0a046ebbb88cb5c769728d94968d6157251ee4ab
jvega190 <josevega19@gmail.com>
2025-08-22 11:02:44 -0600

    [8259] Component data source renders incorrect content type name

--- static-assets/components/cstudio-forms/forms-engine.js
+++ static-assets/components/cstudio-forms/forms-engine.js
@@ -1135,6 +1135,10 @@ const initializeCStudioForms = () => {
 
           CrafterCMSNext.system.getStore().subscribe(() => {
             getInitialConfiguration();
+
+            if (!craftercms.getStore().getState().contentTypes?.byId) {
+              craftercms.getStore().dispatch({ type: 'FETCH_CONTENT_TYPES' });
+            }
           });
         },
 
