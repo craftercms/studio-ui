4f2181be23d9a204d246e83481660a1a55f6e145
jvega190 <josevega19@gmail.com>
2025-08-27 14:51:23 -0600

    [7418] Remove `element` from react render

--- ui/legacy/components/cstudio-admin/mods/content-type-propsheet/contentTypes.js
+++ ui/legacy/components/cstudio-admin/mods/content-type-propsheet/contentTypes.js
@@ -118,8 +118,7 @@
 						const value = (this.value = selected.join(','));
 						updateFn(null, { fieldName: this.fieldName, value });
 					}}
-				/>,
-				element
+				/>
 			);
 			this.value = value;
 		},
