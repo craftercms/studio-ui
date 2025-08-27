e4a5e44cdaf53148c535a82975f9749ecd7b499c
Jose Javier Vega <josevega19@gmail.com>
2025-08-27 06:07:21 -0600

    [4.x] Bump UI deps - yarn dedupe (#4602)
    * Update @typescript-eslint/eslint-plugin, @typescript-eslint-parser, eslint, eslint-plugin-formatjs, eslint-plugin-react-hooks, eslint-react-refresh, @types/eslint, @rollup/plugin-commonjs, @rollup/plugin-node-resolve, @rollup/plugin-replace, @types/node, eslint-config-prettier, eslint-plugin-prettier
    
    * Update glob-gitignore
    
    * Update rimraf, yargs
    
    * Update rollup
    
    * Update @types/jquery, ace-builds, bootstrap, jquery-ui, monaco-editor
    
    * Update babel-plugin-formatjs, @babel/cli, webpack, webpack-cli, @babel/plugin-transform-react-jsx, @babel/preset-env, @babel/preset-react
    
    * Update @emotion/*, @mui/*
    
    * Update @reduxjs/toolkit, @stomp/stompjs, @types/react-swipeable views
    
    * Update moment-timezone, nanoid, notistack, prettier, query-string
    
    * Update react-hotkeys-hook
    
    * Revert "Update @reduxjs/toolkit, @stomp/stompjs, @types/react-swipeable views"
    
    This reverts commit 75e91e54b238732c15a219e735d452b19aae727e.
    
    * Update @stomp/stompjs, @types/react-swipeable-views
    
    * Update tss-react, @formatjs/cli, @types/zxcvbn, rollup/plugin-alias, @types/prop-types
    
    * Update graphql, graphql-ws
    
    * Update marked, marked-highlight
    
    * Update react-intl
    
    * Update typescript
    
    * Update uuid, video.js
    
    * Update @swc/core, @swc/cli
    
    * Update @react-redux
    
    * Update @rollup/plugin-swc, rollup-plugin-swc3
    
    * Update vite, @vitejs/plugin-react, @vitejs/plugin-react-swc
    
    * Update `assert` to `with` to work with node v22
    
    * Revert yargs update
    
    * Remove @types/uuid
    
    * Revert rimraf update
    
    * Move typescript dep to devDependencies
    
    * Add engines to package.json
    
    * eslint config files updates
    
    * Update fast-xml-parser
    
    * Update ecmaVersion and sourceType in eslint.config.mjs
    
    * Yarn dedupe

--- ui/app/src/components/ActivityDashlet/ActivityDashlet.tsx
+++ ui/app/src/components/ActivityDashlet/ActivityDashlet.tsx
@@ -538,6 +538,7 @@ export function ActivityDashlet(props: ActivityDashletProps) {
             </SizedTimelineSeparator>
             <TimelineContent sx={emptyTimelineContentSx} />
           </CustomTimelineItem>
+          {/* @ts-expect-error - TS2786: InfiniteScroll cannot be used as a JSX component. Lib Types are not compatible with the latest react types. */}
           <InfiniteScroll
             initialLoad={false}
             pageStart={0}
