6c20960a158f4238700c2c1baa8f2db3cdab5512
jvega190 <josevega19@gmail.com>
2025-08-27 15:22:08 -0600

    [7418] Null safe handling of datasource.properties

--- ui/app/src/components/ContentTypeManagement/utils.ts
+++ ui/app/src/components/ContentTypeManagement/utils.ts
@@ -698,7 +698,7 @@ function convertDataSourceStructToXmlStruct(
 			//    <properties>
 			//      <enableSearchExisting>true</enableSearchExisting>
 			// TODO: note type usage in `services/contentTypes.ts, parseLegacyFormDefinition when parsing the data sources`
-			property: Object.entries(dataSource.properties).map(([name, value]) => {
+			property: Object.entries(dataSource?.properties ?? []).map(([name, value]) => {
 				let type = descriptor?.fields[name] ? descriptor?.fields[name].type : typeof value;
 				// some properties are simple types, so we need to get the proper type.
 				if (propertiesSimpleTypes.includes(type)) {
