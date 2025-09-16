d67f9ac61bd351a4a8c4be1fb96dc0d2d89c41f6
jvega190 <josevega19@gmail.com>
2025-09-16 15:39:16 -0600

    [8351] Update types, nullish error response validation

--- ui/app/src/components/GroupManagement/GroupManagement.tsx
+++ ui/app/src/components/GroupManagement/GroupManagement.tsx
@@ -37,9 +37,9 @@ export function GroupManagement() {
 	const [offset, setOffset] = useState(0);
 	const [limit, setLimit] = useState(10);
 	const [fetching, setFetching] = useState(false);
-	const [groups, setGroups] = useState<PagedArray<Group>>(null);
-	const [error, setError] = useState<ApiResponse>();
-	const [selectedGroup, setSelectedGroup] = useState<Group>(null);
+	const [groups, setGroups] = useState<PagedArray<Group> | null>(null);
+	const [error, setError] = useState<ApiResponse | null>(null);
+	const [selectedGroup, setSelectedGroup] = useState<Group | null>(null);
 	const [showSearchBox, setShowSearchBox] = useState(false);
 	const [keyword, setKeyword] = useState('');
 
@@ -53,7 +53,7 @@ export function GroupManagement() {
 					setFetching(false);
 				},
 				error({ response }) {
-					setError(response.response);
+					setError(response?.response);
 					setFetching(false);
 				}
 			});
--- ui/app/src/components/UserManagement/UserManagement.tsx
+++ ui/app/src/components/UserManagement/UserManagement.tsx
@@ -43,9 +43,9 @@ export function UserManagement(props: UserManagementProps) {
 	const [offset, setOffset] = useState(0);
 	const [limit, setLimit] = useState(10);
 	const [fetching, setFetching] = useState(false);
-	const [users, setUsers] = useState<PagedArray<User>>(null);
-	const [error, setError] = useState<ApiResponse>();
-	const [viewUser, setViewUser] = useState(null);
+	const [users, setUsers] = useState<PagedArray<User> | null>(null);
+	const [error, setError] = useState<ApiResponse | null>(null);
+	const [viewUser, setViewUser] = useState<User | null>(null);
 	const [showSearchBox, setShowSearchBox] = useState(false);
 	const [keyword, setKeyword] = useState('');
 
@@ -59,7 +59,7 @@ export function UserManagement(props: UserManagementProps) {
 					setFetching(false);
 				},
 				error({ response }) {
-					setError(response.response);
+					setError(response?.response);
 					setFetching(false);
 				}
 			});
