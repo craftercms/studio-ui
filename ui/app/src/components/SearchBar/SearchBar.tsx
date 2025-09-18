849cf7d10ad569d61672aa7a8e7265a3c301bf13
jvega190 <josevega19@gmail.com>
2025-09-18 09:47:51 -0600

    [8352] Make type and initial value consistent

--- ui/app/src/components/SearchBar/SearchBar.tsx
+++ ui/app/src/components/SearchBar/SearchBar.tsx
@@ -122,7 +122,7 @@ export function SearchBar(props: SearchBarProps) {
   const [focus, setFocus] = useState(false);
   const { formatMessage } = useIntl();
   const finalPlaceholder = placeholder || formatMessage(messages.placeholder);
-  const inputRef = useRef<HTMLInputElement | null>(undefined);
+  const inputRef = useRef<HTMLInputElement | null>(null);
   return (
     <Paper
       onClick={onClick}
