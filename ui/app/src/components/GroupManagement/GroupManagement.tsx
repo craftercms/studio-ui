3c35d28940ddfef1eb8d041b3d21524d0fc578c2
Jose Javier Vega <josevega19@gmail.com>
2025-09-19 10:11:20 -0600

    [4.x][8351] User search with [SA] breaks Studio UI (#4635)
    * [8351] Recover from error state on search
    
    * [8351] Update types, nullish error response validation

--- ui/app/src/components/GroupManagement/GroupManagement.tsx
+++ ui/app/src/components/GroupManagement/GroupManagement.tsx
@@ -38,9 +38,9 @@ export function GroupManagement() {
   const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState(10);
   const [fetching, setFetching] = useState(false);
-  const [groups, setGroups] = useState<PagedArray<Group>>(null);
-  const [error, setError] = useState<ApiResponse>();
-  const [selectedGroup, setSelectedGroup] = useState<Group>(null);
+  const [groups, setGroups] = useState<PagedArray<Group> | null>(null);
+  const [error, setError] = useState<ApiResponse | null>(null);
+  const [selectedGroup, setSelectedGroup] = useState<Group | null>(null);
   const [showSearchBox, setShowSearchBox] = useState(false);
   const [keyword, setKeyword] = useState('');
   const { classes, cx: clsx } = useStyles();
@@ -51,10 +51,11 @@ export function GroupManagement() {
       fetchAll({ limit, offset: _offset, keyword }).subscribe({
         next(users) {
           setGroups(users);
+          setError(null);
           setFetching(false);
         },
         error({ response }) {
-          setError(response);
+          setError(response?.response);
           setFetching(false);
         }
       });
--- ui/app/src/components/UserManagement/UserManagement.tsx
+++ ui/app/src/components/UserManagement/UserManagement.tsx
@@ -44,9 +44,9 @@ export function UserManagement(props: UserManagementProps) {
   const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState(10);
   const [fetching, setFetching] = useState(false);
-  const [users, setUsers] = useState<PagedArray<User>>(null);
-  const [error, setError] = useState<ApiResponse>();
-  const [viewUser, setViewUser] = useState(null);
+  const [users, setUsers] = useState<PagedArray<User> | null>(null);
+  const [error, setError] = useState<ApiResponse | null>(null);
+  const [viewUser, setViewUser] = useState<User | null>(null);
   const [showSearchBox, setShowSearchBox] = useState(false);
   const [keyword, setKeyword] = useState('');
   const { classes, cx: clsx } = useStyles();
@@ -57,10 +57,11 @@ export function UserManagement(props: UserManagementProps) {
       fetchAll({ limit, offset: _offset, keyword }).subscribe({
         next(users) {
           setUsers(users);
+          setError(null);
           setFetching(false);
         },
         error({ response }) {
-          setError(response);
+          setError(response?.response);
           setFetching(false);
         }
       });
