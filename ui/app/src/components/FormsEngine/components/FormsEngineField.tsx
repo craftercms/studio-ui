16d700e98495de60d3315998ee6318e2c79bc76a
jvega190 <josevega19@gmail.com>
2025-09-29 12:03:00 -0600

    [7418] Use `getPossibleTranslation`, remove duplicate util translateIfMessageDescriptor

--- ui/app/src/components/FormsEngine/components/FormsEngineField.tsx
+++ ui/app/src/components/FormsEngine/components/FormsEngineField.tsx
@@ -43,7 +43,7 @@ import {
 	useStableGlobalApiContext
 } from '../lib/formsEngineContext';
 import { useAtomValue } from 'jotai';
-import { translateIfMessageDescriptor } from '../lib/formUtils';
+import { getPossibleTranslation } from '../../../utils/i18n';
 
 function createLengthBlock({ length, max, min }: { length: number; max: number; min: number }) {
 	const pieces = [];
@@ -275,7 +275,7 @@ export const FormsEngineField = forwardRef<HTMLDivElement, FormsEngineFieldProps
 			{!isValid &&
 				!!validityData?.messages?.length &&
 				validityData.messages.map((message, key) => (
-					<FormHelperText key={key}>{translateIfMessageDescriptor(message, formatMessage)}</FormHelperText>
+					<FormHelperText key={key}>{getPossibleTranslation(message, formatMessage)}</FormHelperText>
 				))}
 		</FormControl>
 	);
