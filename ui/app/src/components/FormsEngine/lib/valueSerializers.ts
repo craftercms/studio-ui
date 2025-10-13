7fec83148b785c3d587cd64eb0a81c7a7f998978
jvega190 <josevega19@gmail.com>
2025-10-13 16:11:10 -0600

    [7418] Validate undefined properties

--- ui/app/src/components/FormsEngine/lib/valueSerializers.ts
+++ ui/app/src/components/FormsEngine/lib/valueSerializers.ts
@@ -104,7 +104,7 @@ function prepareValuesForXmlSerialising(
 		if (serializer) {
 			jObj[id] = serializer(field, value, contentTypesLookup);
 		}
-		if (field?.properties.tokenize?.value) {
+		if (field?.properties?.tokenize?.value) {
 			fieldAttributes[createAttrHint('tokenized')] = true;
 		}
 		// TODO: Carry/implement attributes (no-default, remote, others?)
