08773286930ca66f0585d1517cfd3ba1b38bb4d5
jvega190 <josevega19@gmail.com>
2025-11-27 12:09:22 -0600

    Update replace-in-file, update guest scripts to be ESM

--- ui/guest/scripts/replaceEnvVersion.js
+++ ui/guest/scripts/replaceEnvVersion.js
@@ -14,14 +14,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const pkg = require('../package.json');
-const path = require('path');
+import pkg from '../package.json' with { type: 'json' };
+import path from 'path';
 const packagePath = process.cwd();
 const buildPath = path.join(packagePath, './build_tsc');
 
-const replace = require('replace-in-file');
+import { replaceInFile } from 'replace-in-file';
 
-replace({
+replaceInFile({
   files: `${buildPath}/**/*.js`,
   from: ['process.env.VERSION', 'process.env.MIN_STUDIO_VERSION'],
   to: [`'${pkg.version}'`, `'${pkg.craftercms.minStudioVersion}'`]
