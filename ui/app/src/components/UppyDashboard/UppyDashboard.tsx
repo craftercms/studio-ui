2a4abffedb0912dc70c62e042b71974b87b5798b
jvega190 <josevega19@gmail.com>
2025-12-15 12:49:45 -0600

    [8441] Add uppy translations

--- ui/app/src/components/UppyDashboard/UppyDashboard.tsx
+++ ui/app/src/components/UppyDashboard/UppyDashboard.tsx
@@ -105,6 +105,15 @@ const translations = defineMessages({
 	},
 	proceedSingle: {
 		defaultMessage: 'Start Upload'
+	},
+	browseFiles: {
+		defaultMessage: 'browse files'
+	},
+	browseFolders: {
+		defaultMessage: 'browse folders'
+	},
+	dropPasteBoth: {
+		defaultMessage: 'Drop files here, {browseFiles} or {browseFolders}'
 	}
 });
 
@@ -185,7 +194,14 @@ export function UppyDashboard(props: UppyDashboardProps) {
 					minimize: formatMessage(translations.minimize),
 					close: formatMessage(translations.close),
 					proceed: formatMessage(translations.proceed),
-					proceedSingle: formatMessage(translations.proceedSingle)
+					proceedSingle: formatMessage(translations.proceedSingle),
+					browseFiles: formatMessage(translations.browseFiles),
+					browseFolders: formatMessage(translations.browseFolders),
+					dropPasteBoth: formatMessage(translations.dropPasteBoth, {
+						// These values are for uppy's mechanism to replace the placeholders with links
+						browseFiles: '%{browseFiles}',
+						browseFolders: '%{browseFolders}'
+					})
 				},
 				pluralize: (n: number) => (n === 1 ? 0 : 1)
 			},
