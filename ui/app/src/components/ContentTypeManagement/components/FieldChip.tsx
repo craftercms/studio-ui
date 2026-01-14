a954c6bcd63982487b892d8238c801b2bee275fc
jvega190 <josevega19@gmail.com>
2026-01-14 08:48:55 -0600

    [8500] Add key to AdditionalFielChip under a map

--- ui/app/src/components/ContentTypeManagement/components/FieldChip.tsx
+++ ui/app/src/components/ContentTypeManagement/components/FieldChip.tsx
@@ -166,7 +166,7 @@ export function FieldChip(props: FieldChipProps) {
 				)}
 			</Root>
 			{additionalFields?.map((additionalFieldId) => (
-				<AdditionalFieldChip fieldId={additionalFieldId} />
+				<AdditionalFieldChip key={additionalFieldId} fieldId={additionalFieldId} />
 			))}
 		</>
 	);
