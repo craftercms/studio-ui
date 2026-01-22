219ff2034f54a0295fd7aa09222991e3100c2672
jvega190 <josevega19@gmail.com>
2026-01-22 14:05:37 -0600

    [8086] Rollback useSaveForm change

--- ui/app/src/components/FormsEngine/lib/useSaveForm.tsx
+++ ui/app/src/components/FormsEngine/lib/useSaveForm.tsx
@@ -32,6 +32,7 @@ import {
 import { FormSavePromiseResult, FormsEngineProps } from '../FormsEngine';
 import { XmlKeys } from './formConsts';
 import { fromString } from '../../../utils/xml';
+import { ensureSingleSlash } from '../../../utils/string';
 import { moveAndUpdateContent, writeContent } from '../../../services/content';
 import { AjaxError } from 'rxjs/ajax';
 import Box from '@mui/material/Box';
