1d3a579c3a3d2aaf3c87066edc035dcd23f09d92
jvega190 <josevega19@gmail.com>
2026-01-30 15:50:32 -0600

    [8534] Update Publish Details Dialog to include more package information

--- ui/app/src/components/PackageDetailsDialog/PublishPackageReview.tsx
+++ ui/app/src/components/PackageDetailsDialog/PublishPackageReview.tsx
@@ -22,6 +22,15 @@ import { PersonAvatar } from '../DashletCard/dashletCommons';
 import { getPersonFullName } from '../../utils/object';
 import ItemPublishingTargetIcon from '../ItemPublishingTargetIcon';
 import React from 'react';
+import Alert from '@mui/material/Alert';
+import VerifiedUserOutlinedIcon from '@mui/icons-material/VerifiedUserOutlined';
+import BlockOutlinedIcon from '@mui/icons-material/BlockOutlined';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { asLocalizedDateTime } from '../../utils/datetime';
+import useLocale from '../../hooks/useLocale';
+import { PACKAGE_APPROVED, PACKAGE_REJECTED } from '../../utils/constants';
 
 export interface PublishPackageReviewProps {
 	publishPackage: PublishPackage;
@@ -34,43 +43,147 @@ const statusItems = {
 
 export function PublishPackageReview(props: PublishPackageReviewProps) {
 	const { publishPackage } = props;
+	const locale = useLocale();
+
+	/* Displaying:
+		- If the package has been reviewed, show the submission date.
+		- If not reviewed but scheduled, show the scheduled date.
+		- If neither, show the published date or the submission date as a fallback.
+	*/
+	const submissionDate = publishPackage.reviewer
+		? publishPackage.submittedOn
+		: (publishPackage.schedule ?? publishPackage.publishedOn ?? publishPackage.submittedOn);
 
 	return (
 		<Box>
-			<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
-				<FormattedMessage defaultMessage="Submitter" />
-			</Typography>
-			<Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
-				<PersonAvatar person={publishPackage.submitter} />
-				<Typography variant="body1" sx={{ ml: 1 }}>
-					{getPersonFullName(publishPackage.submitter)}
+			{publishPackage.approvalState === PACKAGE_REJECTED && (
+				<Alert severity="error" sx={{ mb: 2 }} icon={<BlockOutlinedIcon />}>
+					<FormattedMessage defaultMessage="This package has been rejected." />
+				</Alert>
+			)}
+
+			{/* region Submission details */}
+			<Box sx={{ display: 'block' }}>
+				<Typography variant="subtitle1" gutterBottom sx={{ mb: 1, fontWeight: 'bold' }}>
+					<FormattedMessage defaultMessage="Submission details" />
 				</Typography>
+				<Paper sx={{ p: 1.5 }}>
+					<Grid container spacing={2}>
+						<Grid size={6}>
+							<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+								<FormattedMessage defaultMessage="Package title" />
+							</Typography>
+							<Typography>{publishPackage.title}</Typography>
+						</Grid>
+						<Grid size={6}>
+							<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+								<FormattedMessage defaultMessage="Submitter" />
+							</Typography>
+							<Stack direction="row" spacing={1} alignItems="center">
+								<PersonAvatar person={publishPackage.submitter} />
+								<Typography variant="body1" sx={{ ml: 1 }}>
+									{getPersonFullName(publishPackage.submitter)}
+								</Typography>
+							</Stack>
+						</Grid>
+						<Grid size={6}>
+							<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+								<FormattedMessage defaultMessage="Publishing Target" />
+							</Typography>
+							<Stack direction="row" spacing={1} alignItems="center">
+								<ItemPublishingTargetIcon item={statusItems[publishPackage.target] as ContentItem} />
+								{publishPackage.target === 'live' ? (
+									<FormattedMessage defaultMessage="Live" />
+								) : (
+									<FormattedMessage defaultMessage="Staging" />
+								)}
+							</Stack>
+						</Grid>
+						<Grid size={6}>
+							<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+								{publishPackage.reviewer ? (
+									<FormattedMessage defaultMessage="Submitted On" />
+								) : (
+									<FormattedMessage defaultMessage="Scheduled For" />
+								)}
+							</Typography>
+							<Typography>
+								{asLocalizedDateTime(submissionDate, locale.localeCode, locale.dateTimeFormatOptions)}
+							</Typography>
+						</Grid>
+						<Grid size={12}>
+							<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+								<FormattedMessage defaultMessage="Comment" />
+							</Typography>
+							{publishPackage.submitterComment || <FormattedMessage defaultMessage="No submission comment provided" />}
+						</Grid>
+					</Grid>
+				</Paper>
 			</Box>
-			<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
-				<FormattedMessage defaultMessage="Package Title" />
-			</Typography>
-			<Typography variant="body1" sx={{ mb: 2 }}>
-				{publishPackage.title}
-			</Typography>
-			<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
-				<FormattedMessage defaultMessage="Submission Comment" />
-			</Typography>
-			<Typography variant="body1" sx={{ mb: 2 }}>
-				{publishPackage.submitterComment || <FormattedMessage defaultMessage="No submission comment provided" />}
-			</Typography>
-			<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
-				<FormattedMessage defaultMessage="Publishing Target" />
-			</Typography>
-			<Box sx={{ display: 'flex', alignItems: 'center', mb: 2, gap: 1 }}>
-				<ItemPublishingTargetIcon item={statusItems[publishPackage.target] as ContentItem} />
-				<Typography variant="body1" component="span">
-					{publishPackage.target === 'live' ? (
-						<FormattedMessage defaultMessage="Live" />
-					) : (
-						<FormattedMessage defaultMessage="Staging" />
-					)}
-				</Typography>
-			</Box>
+			{/* endregion */}
+
+			{/* region Review details */}
+			{Boolean(publishPackage.reviewer) && (
+				<Box sx={{ mt: 2 }}>
+					<Typography variant="subtitle1" fontWeight="bold" sx={{ mb: 1 }}>
+						<FormattedMessage defaultMessage="Review details" />
+					</Typography>
+					<Paper sx={{ p: 1.5 }}>
+						<Grid container spacing={2}>
+							<Grid size={6}>
+								<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+									<FormattedMessage defaultMessage="Status" />
+								</Typography>
+								<Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+									{publishPackage.approvalState === PACKAGE_APPROVED ? (
+										<>
+											<VerifiedUserOutlinedIcon color="success" fontSize="small" sx={{ mr: 1 }} />
+											<FormattedMessage defaultMessage="Approved" />
+										</>
+									) : (
+										<>
+											<BlockOutlinedIcon color="error" fontSize="small" sx={{ mr: 1 }} />
+											<FormattedMessage defaultMessage="Rejected" />
+										</>
+									)}
+								</Box>
+							</Grid>
+							<Grid size={6}>
+								<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+									<FormattedMessage defaultMessage="Reviewer" />
+								</Typography>
+								<Box sx={{ mb: 2 }}>
+									<Typography variant="body1">{getPersonFullName(publishPackage.reviewer)}</Typography>
+								</Box>
+							</Grid>
+							<Grid size={12}>
+								{publishPackage.approvalState === PACKAGE_APPROVED ? (
+									<>
+										<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+											<FormattedMessage defaultMessage="Comment" />
+										</Typography>
+										<Typography variant="body1" sx={{ mb: 2 }}>
+											{publishPackage.reviewerComment || <FormattedMessage defaultMessage="No reason provided" />}
+										</Typography>
+									</>
+								) : (
+									<>
+										<Typography variant="body2" color="textSecondary" sx={{ mb: 1 }}>
+											<FormattedMessage defaultMessage="Reason" />
+										</Typography>
+										<Typography variant="body1" sx={{ mb: 2 }}>
+											{publishPackage.reviewerComment || (
+												<FormattedMessage defaultMessage="No approval comment provided" />
+											)}
+										</Typography>
+									</>
+								)}
+							</Grid>
+						</Grid>
+					</Paper>
+				</Box>
+			)}
+			{/* endregion */}
 		</Box>
 	);
 }
