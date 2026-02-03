1e12fc05e7b38c3bd9a939e775d9f4b2980522dd
jvega190 <josevega19@gmail.com>
2026-02-03 16:32:15 -0600

    [8540] Reverting a page's controller or template not refreshing preview

--- ui/app/src/state/epics/itemVersions.ts
+++ ui/app/src/state/epics/itemVersions.ts
@@ -43,6 +43,8 @@ import { batchActions } from '../actions/misc';
 import { getHostToGuestBus } from '../../utils/subjects';
 import { reloadRequest } from '../actions/preview';
 import { CrafterCMSEpic } from '../store';
+import { getControllerPath } from '../../utils/path';
+import { popPiece } from '../../utils/string';
 
 export default [
 	(action$, state$: StateObservable<GlobalState>) =>
@@ -92,7 +94,23 @@ export default [
 			ofType(revertContentComplete.type),
 			withLatestFrom(state$),
 			switchMap(([{ payload }, state]) => {
-				if (payload.path === state.preview.guest?.path) {
+				const currentPreviewPath = state.preview.guest?.path;
+				let isPreviewTemplateOrController = false;
+				if (currentPreviewPath) {
+					const currentItem = state.content.itemsByPath[currentPreviewPath];
+					const currentContentTypeId = currentItem?.contentTypeId;
+					const currentContentType = currentContentTypeId ? state.contentTypes.byId[currentContentTypeId] : null;
+					const controllerPath =
+						currentContentTypeId && currentItem
+							? `${getControllerPath(currentItem.systemType)}${popPiece(currentContentTypeId, '/')}.groovy}`
+							: null;
+
+					if (currentContentType?.displayTemplate === payload.path || controllerPath === payload.path) {
+						isPreviewTemplateOrController = true;
+					}
+				}
+
+				if (payload.path === state.preview.guest?.path || isPreviewTemplateOrController) {
 					getHostToGuestBus().next({ type: reloadRequest.type });
 				}
 				return of(
