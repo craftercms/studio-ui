b03ee1a405ba8cf616b8fa6094f53ddb820a7ab3
jvega190 <josevega19@gmail.com>
2026-02-11 09:16:47 -0600

    [8358] Fix childrenByParentPath validation to handle undefined cases

--- ui/app/src/components/PathNavigatorTree/PathNavigatorTreeItem.tsx
+++ ui/app/src/components/PathNavigatorTree/PathNavigatorTreeItem.tsx
@@ -187,7 +187,7 @@ export function PathNavigatorTreeItem(props: PathNavigatorTreeItemProps) {
 					</Button>
 				</Box>
 			);
-	} else if (totalByPath[path] > 0 && !childrenByParentPath[path].length) {
+	} else if (totalByPath[path] > 0 && !childrenByParentPath[path]?.length) {
 		// If totalByPath at the current path is greater than 0, but there are no children in childrenByParentPath for the current path,
 		// it means that the children are still loading, so we show a loading indicator. If there is an error for the current path, we show an error message instead.
 		propsForTreeItem.children.push(
