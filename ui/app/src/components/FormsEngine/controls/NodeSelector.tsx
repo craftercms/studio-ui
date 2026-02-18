557e8731a26531a63147391716b004de224378b1
jvega190 <josevega19@gmail.com>
2026-02-18 15:16:32 -0600

    [7123] Search and Browse dialogs should either hide existing selected items or show them as already selected

--- ui/app/src/components/FormsEngine/controls/NodeSelector.tsx
+++ ui/app/src/components/FormsEngine/controls/NodeSelector.tsx
@@ -482,6 +482,7 @@ function NodeSelector(props: NodeSelectorProps) {
 					dispatch,
 					path: processPath(pickerChoice.path),
 					contentTypes: pickerChoice.allowedContentTypes,
+					preselectedPaths: value.map((item) => item.key).filter(Boolean),
 					onSuccess(items: MediaItem | MediaItem[]) {
 						const nextValue = value.concat();
 						asArray(items).forEach((item) => {
@@ -504,6 +505,7 @@ function NodeSelector(props: NodeSelectorProps) {
 					dispatch,
 					path: ensureSingleSlash(`${processPath(pickerChoice.path)}/.+`),
 					contentTypes: pickerChoice.allowedContentTypes,
+					preselectedPaths: value.map((item) => item.key).filter(Boolean),
 					onAcceptSelection(paths, items) {
 						const nextValue = value.concat();
 						items?.forEach((item) => {
