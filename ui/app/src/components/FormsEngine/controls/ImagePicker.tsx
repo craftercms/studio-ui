dd867d8ec63f35056a04b2bae770ffa9cbdc8ed8
Jose Javier Vega <josevega19@gmail.com>
2026-02-26 16:23:01 -0600

    [7123] Search and Browse dialogs should either hide existing selected items or show them as already selected (#4810)
    * [7123] Search and Browse dialogs should either hide existing selected items or show them as already selected
    
    * [7123] avoid array of empty string

--- ui/app/src/components/FormsEngine/controls/ImagePicker.tsx
+++ ui/app/src/components/FormsEngine/controls/ImagePicker.tsx
@@ -204,6 +204,7 @@ export function ImagePicker(props: ImagePickerProps) {
 					dispatch,
 					path: processPath(choice.path),
 					multiSelect: false,
+					preselectedPaths: value ? [value] : [],
 					onSuccess(imageData: MediaItem) {
 						// Check if the image meets restrictions
 						validateImageRestrictions(imageData.path, restrictions).then((meetsRestrictions) => {
@@ -230,6 +231,7 @@ export function ImagePicker(props: ImagePickerProps) {
 				showSearchDialog({
 					dispatch,
 					path: ensureSingleSlash(`${processPath(choice.path)}/.+`),
+					preselectedPaths: value ? [value] : [],
 					onAcceptSelection(images) {
 						validateImageRestrictions(images[0], restrictions).then((meetsRestrictions) => {
 							if (!meetsRestrictions) {
