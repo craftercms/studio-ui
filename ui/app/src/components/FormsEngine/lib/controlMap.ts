427021917a18f61866d4890707a507cd5c8c3ca3
jvega190 <josevega19@gmail.com>
2026-03-03 08:26:25 -0600

    [7418] Forcehttps control

--- ui/app/src/components/FormsEngine/lib/controlMap.ts
+++ ui/app/src/components/FormsEngine/lib/controlMap.ts
@@ -58,7 +58,7 @@ export const controlMap: Record<BuiltInControlType, ElementType> = {
 	disabled: null,
 	dropdown: lazy(() => import('../controls/Dropdown')),
 	'file-name': lazy(() => import('../controls/./FileName')),
-	forcehttps: null, // TODO: probably not needed, getname returns `disabled`
+	forcehttps: lazy(() => import('../controls/Checkbox')),
 	'image-picker': lazy(() => import('../controls/ImagePicker')),
 	input: lazy(() => import('../controls/Text')),
 	'internal-name': null,
