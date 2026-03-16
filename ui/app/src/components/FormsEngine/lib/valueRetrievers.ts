a00ae8ab33fb3e4de519ed4c90f7a413c4f98543
Jose Javier Vega <josevega19@gmail.com>
2026-03-16 11:05:22 -0600

    [7418] Uuid control (#4819)

--- ui/app/src/components/FormsEngine/lib/valueRetrievers.ts
+++ ui/app/src/components/FormsEngine/lib/valueRetrievers.ts
@@ -23,6 +23,7 @@ import { systemFieldsNotInType, XmlKeys } from './formConsts';
 import { deserialize, unescapeXml } from '../../../utils/xml';
 import type { DescriptorControlType } from '../../ContentTypeManagement/controlMap';
 import { nnou } from '../../../utils/object';
+import { v4 as uuid } from 'uuid';
 
 export type ValueRetriever<T = unknown> = (value: unknown, field: ContentTypeField) => T;
 
@@ -56,7 +57,7 @@ export const valueRetrieverLookup: Record<BuiltInControlType | DescriptorControl
 	textarea: textFieldExtractor,
 	time: null,
 	'transcoded-video-picker': textFieldExtractor,
-	uuid: textFieldExtractor,
+	uuid: uuidExtractor,
 	'video-picker': textFieldExtractor,
 	colorPicker: textOrNullExtractor,
 	'content-path-input': textFieldExtractor,
@@ -220,4 +221,8 @@ export function objectExtractor(value: string): object {
 		console.error('Invalid JSON', e);
 		return {};
 	}
+}
+
+export function uuidExtractor(value: unknown): string {
+	return textOrNullExtractor(value) ?? uuid();
 }
