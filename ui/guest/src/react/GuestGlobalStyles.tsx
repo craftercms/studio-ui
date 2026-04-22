d7fb0f0435d0bc7c464e02ab75c18c09eb5e9cf7
jvega190 <josevega19@gmail.com>
2026-04-22 14:45:16 -0600

    Update dropzone placeholder text for clarity

--- ui/guest/src/react/GuestGlobalStyles.tsx
+++ ui/guest/src/react/GuestGlobalStyles.tsx
@@ -181,7 +181,7 @@ export function GuestGlobalStyles(props: GuestGlobalStylesProps) {
 										color: '#fff',
 										display: 'inline-block',
 										padding: '10px',
-										content: '"Empty component dropzone. Drag new or existing content items to zone to add them"',
+										content: '"Empty component dropzone. Drag new or existing content items here to add them"',
 										fontWeight: 'bold'
 									}
 								},
