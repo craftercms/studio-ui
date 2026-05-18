6bbef071eaabc7d6ee67599b8785951bb89d0314
jvega190 <josevega19@gmail.com>
2026-05-18 10:29:26 -0600

    [6601] Update ImagePicker to allow passing initialParameters to BrowseFilesDialog

--- ui/app/src/components/FormsEngine/lib/controlHelpers.tsx
+++ ui/app/src/components/FormsEngine/lib/controlHelpers.tsx
@@ -267,7 +267,8 @@ export const showBrowseFilesDialog = ({
 	path,
 	contentTypes,
 	multiSelect = true,
-	preselectedPaths = []
+	preselectedPaths = [],
+	initialParameters = {}
 }: {
 	path: string;
 	dispatch: ReduxDispatch;
@@ -275,6 +276,7 @@ export const showBrowseFilesDialog = ({
 	contentTypes?: string[];
 	multiSelect?: boolean;
 	preselectedPaths?: string[];
+	initialParameters?: BrowseFilesDialogProps['initialParameters'];
 }): void => {
 	const id = nanoid();
 	dispatch(
@@ -287,6 +289,7 @@ export const showBrowseFilesDialog = ({
 				allowUpload: false,
 				contentTypes: contentTypes ?? [],
 				preselectedPaths,
+				initialParameters,
 				onClose: () => dispatch(popDialog({ id })),
 				onSuccess(items) {
 					dispatch(popDialog({ id }));
