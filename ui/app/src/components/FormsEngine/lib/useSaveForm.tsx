6ed0e57fd3b0fecf5fdba1e9398ccd0b0cdd2e81
jvega190 <josevega19@gmail.com>
2026-05-21 16:04:25 -0600

    Update FormsEngine hook useSaveForm to add comment to cancel package in viewPackagesDialog

--- ui/app/src/components/FormsEngine/lib/useSaveForm.tsx
+++ ui/app/src/components/FormsEngine/lib/useSaveForm.tsx
@@ -209,15 +209,14 @@ export function useSaveForm(props: UseSaveFormProps) {
 		}
 
 		// TODO: write-content url on FE1 sends phase, path, fileName, contentType QSAs. Important?
-		const saveContent = () => {
+		const saveContent = (cancelPackagesComment: string = '') => {
 			const saveOrMoveService$ = isRename
 				? moveAndUpdateContent(siteId, itemPath, path, xml)
 				: writeContent(siteId, path, xml);
 			const saveOrCancel$ = affectedPackages?.length
 				? cancelPackages(siteId, {
 						packageIds: affectedPackages.map((pkg) => pkg.id),
-						// TODO: Correct comment generation
-						comment: `Cancel packages to write on "${path}`
+						comment: cancelPackagesComment
 					}).pipe(
 						switchMap(() => {
 							return saveOrMoveService$;
@@ -238,8 +237,12 @@ export function useSaveForm(props: UseSaveFormProps) {
 						component: createComponentId('ViewPackagesDialog'),
 						props: {
 							item,
-							onContinue: () => {
-								saveContent();
+							cancelPackagesInitialComment: formatMessage(
+								{ defaultMessage: 'Cancel packages to write on "{path}"' },
+								{ path }
+							),
+							onContinue: (cancelPackagesUpdatedComment) => {
+								saveContent(cancelPackagesUpdatedComment);
 								dispatch(popDialog({ id: dialogId }));
 							},
 							onClose: () => {
