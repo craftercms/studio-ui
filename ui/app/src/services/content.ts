62886ade344cefccd67c5f563be1a776bf8c4402
jvega190 <josevega19@gmail.com>
2026-06-10 16:20:52 -0600

    Add comments to content save/write operations - XB

--- ui/app/src/services/content.ts
+++ ui/app/src/services/content.ts
@@ -53,6 +53,7 @@ import { v4 as uuid } from 'uuid';
 import FetchItemsByPathArray from '../models/FetchItemsByPathArray';
 import { isMediaContent, isTextContent } from '../components/PathNavigator/utils';
 import { fromPromise } from 'rxjs/internal/observable/innerFrom';
+import { getCurrentIntl } from '../utils/i18n';
 
 export function fetchComponentInstanceHTML(path: string): Observable<string> {
 	return getText(`/crafter-controller/component.html${toQueryString({ path })}`).pipe(pluck('response'));
@@ -246,6 +247,10 @@ export function updateField(
 	value: any,
 	serializeValue: boolean | ((value: any) => string) = false
 ): Observable<any> {
+	const writeContentComment = getCurrentIntl().formatMessage(
+		{ defaultMessage: 'Updated {fieldId} in {path}' },
+		{ fieldId, path }
+	);
 	return performMutation(
 		site,
 		path,
@@ -272,7 +277,8 @@ export function updateField(
 						? cdataWrap(value)
 						: value;
 		},
-		modelId
+		modelId,
+		writeContentComment
 	);
 }
 // endregion
@@ -282,7 +288,8 @@ function performMutation(
 	site: string,
 	path: string,
 	mutation: (doc: Element) => void,
-	modelId: string = null
+	modelId: string = null,
+	writeContentComment?: string
 ): Observable<{ updatedDocument: XMLDocument }> {
 	return fetchContentDOM(site, path).pipe(
 		switchMap((doc) => {
@@ -298,7 +305,11 @@ function performMutation(
 			updateModifiedDateElement(doc.documentElement);
 
 			return fromPromise(beautify(serialize(doc))).pipe(
-				switchMap((xml) => writeContent(site, path, xml, { unlock: true }).pipe(map(() => ({ updatedDocument: doc }))))
+				switchMap((xml) =>
+					writeContent(site, path, xml, { unlock: true, comment: writeContentComment }).pipe(
+						map(() => ({ updatedDocument: doc }))
+					)
+				)
 			);
 		})
 	);
@@ -322,6 +333,10 @@ export function insertComponent(
 	isSharedInstance = false,
 	shouldSerializeValueFn?: (fieldId: string) => boolean
 ): Observable<any> {
+	const writeContentComment = getCurrentIntl().formatMessage(
+		{ defaultMessage: 'Inserted component {label} in {path}' },
+		{ label: insertedContentInstance.craftercms.label, path: parentDocPath }
+	);
 	return performMutation(
 		siteId,
 		parentDocPath,
@@ -350,7 +365,8 @@ export function insertComponent(
 
 			insertCollectionItem(element, parentFieldId, targetIndex, newItem);
 		},
-		parentModelId
+		parentModelId,
+		writeContentComment
 	);
 }
 // endregion
@@ -369,6 +385,10 @@ export function insertInstance(
 	insertedInstance: ContentInstance,
 	datasource?: string
 ): Observable<any> {
+	const writeContentComment = getCurrentIntl().formatMessage(
+		{ defaultMessage: 'Inserted instance {label} in {path}' },
+		{ label: insertedInstance.craftercms.label, path: parentDocPath }
+	);
 	return performMutation(
 		siteId,
 		parentDocPath,
@@ -390,7 +410,8 @@ export function insertInstance(
 
 			insertCollectionItem(element, parentFieldId, targetIndex, newItem);
 		},
-		parentModelId
+		parentModelId,
+		writeContentComment
 	);
 }
 // endregion
@@ -494,16 +515,24 @@ export function duplicateItem(
 						updateComponentId(componentDoc.documentElement, newItemData.modelId);
 						updateModifiedDateElement(componentDoc.documentElement);
 						updateCreatedDateElement(componentDoc.documentElement);
+						const writeContentComment = getCurrentIntl().formatMessage(
+							{ defaultMessage: 'Duplicated item {itemPath} in {path}' },
+							{ itemPath, path }
+						);
 						return forkJoin([
 							// Write the main document.
 							fromPromise(beautify(serialize(doc))).pipe(
-								switchMap((xml) => writeContent(site, path, xml, { unlock: true }))
+								switchMap((xml) => writeContent(site, path, xml, { unlock: true, comment: writeContentComment }))
 							),
 							// Write the new/duplicated shared component.
 							fromPromise(beautify(serialize(componentDoc))).pipe(
 								switchMap((xml) =>
 									writeContent(site, `${returnValue.newItem.path}/${returnValue.newItem.modelId}.xml`, xml, {
-										unlock: true
+										unlock: true,
+										comment: getCurrentIntl().formatMessage(
+											{ defaultMessage: 'Created duplicated shared component {path}/{modelId}.xml' },
+											{ path: returnValue.newItem.path, modelId: returnValue.newItem.modelId }
+										)
 									})
 								)
 							)
@@ -530,6 +559,10 @@ export function sortItem(
 	targetIndex: number,
 	path: string
 ): Observable<any> {
+	const writeContentComment = getCurrentIntl().formatMessage(
+		{ defaultMessage: 'Moved item from index {currentIndex} to {targetIndex} in {fieldId}. Path: {path}' },
+		{ currentIndex, targetIndex, fieldId, path }
+	);
 	return performMutation(
 		site,
 		path,
@@ -537,7 +570,8 @@ export function sortItem(
 			const item = extractNode(element, fieldId, currentIndex);
 			insertCollectionItem(element, fieldId, targetIndex, item, currentIndex);
 		},
-		modelId
+		modelId,
+		writeContentComment
 	);
 }
 // endregion
@@ -562,35 +596,46 @@ export function moveItem(
 	const isSameModel = originalModelId === targetModelId;
 	const isSameDocument = originalParentPath === targetParentPath;
 	if (isSameDocument || isSameModel) {
+		const writeContentComment = getCurrentIntl().formatMessage(
+			{ defaultMessage: 'Moving item {originalFieldId} from {originalIndex} to {targetIndex} in {originalParentPath}' },
+			{ originalFieldId, originalIndex, targetIndex, originalParentPath }
+		);
 		// Moving items between two fields of the same document or model...
-		return performMutation(site, originalParentPath, (element) => {
-			// Item may be moving...
-			// - from parent model to an embedded model
-			// - from an embedded model to the parent model
-			// - from an embedded model to another embedded model
-			// - from a field to another WITHIN the same model (parent or embedded)
-			const parentDocumentModelId = getInnerHtml(element.querySelector(':scope > objectId'));
-			const sourceModelElement =
-				parentDocumentModelId === originalModelId ? element : element.querySelector(`[id="${originalModelId}"]`);
-			const targetModelElement =
-				parentDocumentModelId === targetModelId ? element : element.querySelector(`[id="${targetModelId}"]`);
-			const item = extractNode(sourceModelElement, originalFieldId, originalIndex);
-			let targetField = extractNode(targetModelElement, targetFieldId, removeLastPiece(`${targetIndex}`));
-			if (!targetField) {
-				const newField = createElement(originalFieldId);
-				newField.setAttribute('item-list', 'true');
-				targetModelElement.appendChild(newField);
-				targetField = newField;
-			}
-			const targetFieldItems = targetField.querySelectorAll(':scope > item');
-			const parsedTargetIndex = parseInt(popPiece(`${targetIndex}`));
-			if (targetFieldItems.length === parsedTargetIndex) {
-				targetField.appendChild(item);
-			} else {
-				targetField.insertBefore(item, targetFieldItems[parsedTargetIndex]);
-			}
-		});
+		return performMutation(
+			site,
+			originalParentPath,
+			(element) => {
+				// Item may be moving...
+				// - from parent model to an embedded model
+				// - from an embedded model to the parent model
+				// - from an embedded model to another embedded model
+				// - from a field to another WITHIN the same model (parent or embedded)
+				const parentDocumentModelId = getInnerHtml(element.querySelector(':scope > objectId'));
+				const sourceModelElement =
+					parentDocumentModelId === originalModelId ? element : element.querySelector(`[id="${originalModelId}"]`);
+				const targetModelElement =
+					parentDocumentModelId === targetModelId ? element : element.querySelector(`[id="${targetModelId}"]`);
+				const item = extractNode(sourceModelElement, originalFieldId, originalIndex);
+				let targetField = extractNode(targetModelElement, targetFieldId, removeLastPiece(`${targetIndex}`));
+				if (!targetField) {
+					const newField = createElement(originalFieldId);
+					newField.setAttribute('item-list', 'true');
+					targetModelElement.appendChild(newField);
+					targetField = newField;
+				}
+				const targetFieldItems = targetField.querySelectorAll(':scope > item');
+				const parsedTargetIndex = parseInt(popPiece(`${targetIndex}`));
+				if (targetFieldItems.length === parsedTargetIndex) {
+					targetField.appendChild(item);
+				} else {
+					targetField.insertBefore(item, targetFieldItems[parsedTargetIndex]);
+				}
+			},
+			null,
+			writeContentComment
+		);
 	} else {
+		const writeContentComment = `Moving item ${originalFieldId} from ${originalIndex} to ${targetIndex} in ${originalParentPath} to ${targetParentPath}`;
 		let removedItemHTML: string;
 		return performMutation(
 			site,
@@ -602,7 +647,8 @@ export function moveItem(
 				removedItemHTML = item.outerHTML;
 				field.removeChild(item);
 			},
-			originalModelId
+			originalModelId,
+			writeContentComment
 		).pipe(
 			switchMap(() =>
 				performMutation(
@@ -640,6 +686,10 @@ export function deleteItem(
 	indexToDelete: number | string,
 	path: string
 ): Observable<any> {
+	const writeContentComment = getCurrentIntl().formatMessage(
+		{ defaultMessage: 'Removed item {fieldId} at index {indexToDelete} in {path}' },
+		{ fieldId, indexToDelete, path }
+	);
 	return performMutation(
 		site,
 		path,
@@ -664,7 +714,8 @@ export function deleteItem(
 				fieldNode.innerHTML = '';
 			}
 		},
-		modelId
+		modelId,
+		writeContentComment
 	);
 }
 // endregion
