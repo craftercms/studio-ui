9e93b39c1e908f6398d3a80c7b521004e71046b6
jvega190 <josevega19@gmail.com>
2026-06-18 08:44:24 -0600

    Use default comment if cancelPackagesComment is ane empty string

--- ui/guest/src/store/util.ts
+++ ui/guest/src/store/util.ts
@@ -81,7 +81,7 @@ export function beforeWrite$<T extends any = 'continue', S extends any = never>(
 						payload.type === 'continue'
 							? cancelPackages(site, {
 									packageIds: affectedPackages.map((p) => p.id),
-									comment: payload.cancelPackagesComment ?? `Cancel packages to write on "${path}"`
+									comment: payload.cancelPackagesComment || `Cancel packages to write on "${path}"`
 								}).pipe(switchMap(() => continue$))
 							: stop$
 					)
