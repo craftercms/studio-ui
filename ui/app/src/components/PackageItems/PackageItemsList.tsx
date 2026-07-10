13ac2439bba4afe3ee3769b2871f99e5701d5469
jvega190 <josevega19@gmail.com>
2026-07-10 11:26:36 -0600

    Update PackageItems container to handle scrollbar only in tree view, leave list view scroll to reac-window

--- ui/app/src/components/PackageItems/PackageItemsList.tsx
+++ ui/app/src/components/PackageItems/PackageItemsList.tsx
@@ -53,14 +53,13 @@ export function PackageItemsList(props: PackageItemsListProps) {
 	});
 
 	return (
-		<Box sx={{ flex: 1 }}>
+		<Box sx={{ flex: 1, minHeight: 0 }}>
 			<List
 				className="List"
 				rowCount={rowCount}
 				rowHeight={59}
 				onRowsRendered={onRowsRendered}
 				rowProps={{ rows: items }}
-				style={{ overflow: 'hidden' }} // The wrapper component already has overflow: auto, so we need to hide it here to avoid double scrollbar
 				rowComponent={({ index, style }: RowComponentProps) => {
 					let content;
 					if (!isItemLoaded(index)) {
