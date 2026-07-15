213d59c1472e34a28df7d9881fee1ed6f75a42cf
jvega190 <josevega19@gmail.com>
2026-07-15 09:25:37 -0600

    Prevent 0-second snackbar durations

--- ui/app/src/components/AccountManagement/AccountManagement.tsx
+++ ui/app/src/components/AccountManagement/AccountManagement.tsx
@@ -403,8 +403,13 @@ export function AccountManagement(props: AccountManagementProps) {
 							<NumberField.Root
 								id="snackDuration"
 								value={snackDuration / 1000} // Display in seconds
-								onValueChange={(value) => setSnackDuration(value * 1000)} // Store in milliseconds
-								min={0}
+								onValueChange={(value) => {
+									const seconds = Number(value);
+									setSnackDuration(
+										Number.isFinite(seconds) && seconds > 0 ? seconds * 1000 : DEFAULT_SNACKBAR_DURATION
+									);
+								}} // Store in milliseconds
+								min={1}
 								max={60}
 								step={1}
 								format={{ maximumFractionDigits: 0 }}
@@ -426,9 +431,7 @@ export function AccountManagement(props: AccountManagementProps) {
 
 						<FormControl sx={{ my: 2 }}>
 							<FormControlLabel
-								control={
-									<Switch checked={enableAnimations} onChange={(e) => setEnableAnimations(e.target.checked)} />
-								}
+								control={<Switch checked={enableAnimations} onChange={(e) => setEnableAnimations(e.target.checked)} />}
 								label={<FormattedMessage defaultMessage="Enable user interface animations" />}
 							/>
 						</FormControl>
